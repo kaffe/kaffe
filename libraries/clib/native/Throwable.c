656645af25073aee84c7c4d93621acbc429ee70a
tim <tim>
1998-04-04 23:19:07 +0000

    Added new stack trace system which is much more efficient compared to the old one.  We now only build the backtrace information in humand readable form if we actually want to print it.

--- libraries/clib/native/Throwable.c
+++ libraries/clib/native/Throwable.c
@@ -18,6 +18,7 @@
 #include "../../../kaffe/kaffevm/support.h"
 
 extern Hjava_lang_Object* buildStackTrace(void*);
+extern void printStackTrace(struct Hjava_lang_Throwable*, struct Hjava_lang_Object*);
 
 /*
  * Fill in stack trace information - don't know what thought.
@@ -35,17 +36,5 @@ java_lang_Throwable_fillInStackTrace(struct Hjava_lang_Throwable* o)
 void
 java_lang_Throwable_printStackTrace0(struct Hjava_lang_Throwable* o, struct Hjava_lang_Object* p)
 {
-	int i;
-	HArrayOfArray* strarray;
-
-	strarray = (HArrayOfArray*)unhand(o)->backtrace;
-	if (strarray != 0) {
-		for (i = 0; i < obj_length(strarray); i++) {
-			Hjava_lang_Object* line = unhand(strarray)->body[i];
-			if (line != 0) {
-				do_execute_java_method(p, "println", "([C)V", 0, 0, line);
-			}
-		}
-		do_execute_java_method(p, "flush", "()V", 0, 0);
-	}
+	printStackTrace(o, p);
 }
