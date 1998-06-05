c496a5da4db4c3b60137293ea6af602aa9b010ec
cvs-gback <cvs-gback>
1998-06-05 02:15:46 +0000

    added preliminary arm32 port.

---
+++ config/arm/netbsd1/md.h
@@ -0,0 +1,22 @@
+/*
+ * arm32/netbsd1/md.h
+ * NetBSD/arm32 configuration information.
+ *
+ * Copyright (c) 1996, 1997
+ *	Transvirtual Technologies, Inc.  All rights reserved.
+ *
+ * See the file "license.terms" for information on usage and redistribution 
+ * of this file. 
+ */
+
+#ifndef __arm32_netbsd1_md_h
+#define __arm32_netbsd1_md_h
+
+#include "arm32/common.h"
+#include "arm32/threads.h"
+
+#if defined(TRANSLATOR)
+#include "jit-md.h"
+#endif
+
+#endif
