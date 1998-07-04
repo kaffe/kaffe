624acaffe99d048a736c5debb568597a67af2f1a
cvs-gback <cvs-gback>
1998-07-04 00:53:51 +0000

    added TestSerializable.java

---
+++ test/regression/TestSerializable.java
@@ -0,0 +1,26 @@
+/*
+ * This class tests that 
+ * - we do not report java.io.Serializable for arrays 
+ * - we report the same array modifiers as Sun
+ * - we implement isAssignableFrom properly for classes and arrays
+ */
+public class TestSerializable
+{
+    class help implements java.io.Serializable
+    {
+    }
+
+    public static void main(String av[])
+    {
+	Class c = TestSerializable[].class;
+	System.out.println(c.getInterfaces().length);
+	System.out.println(c.getModifiers());
+	Class s = java.io.Serializable.class;
+	System.out.println(s.isAssignableFrom(help.class));
+	System.out.println(s.isAssignableFrom(help[].class));
+	System.out.println(s.isAssignableFrom(TestSerializable.class));
+	System.out.println(s.isAssignableFrom(TestSerializable[].class));
+	System.out.println(s.isAssignableFrom(boolean.class));
+	System.out.println(s.isAssignableFrom(boolean[].class));
+    }
+}
