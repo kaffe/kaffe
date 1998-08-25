391cd024a8949a261b5d9c7558cdd43b70c62159
gback <gback>
1998-08-25 01:15:11 +0000

    added simple int/char conversion test

---
+++ test/regression/CharCvt.java
@@ -0,0 +1,32 @@
+/**
+ * this class tests some aspects of the conversion 
+ * between ints, shorts, and chars
+ */
+public class CharCvt
+{
+    static void print_as_int(char x)
+    {
+	System.out.println((int)x);
+    }
+
+    static void print_as_int(short x)
+    {
+	System.out.println((int)x);
+    }
+
+    static char make_a_char(int x)
+    {
+	return (char)x;
+    }
+
+    static short make_a_short(int x)
+    {
+	return (short)x;
+    }
+
+    public static void main(String av[])
+    {
+	print_as_int(make_a_char(-1));
+	print_as_int(make_a_short(-1));
+    }
+}
