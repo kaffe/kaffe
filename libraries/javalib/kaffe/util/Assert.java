b7f7df36eb5e6d9725604d261cbced58d651bf23
archie <archie>
1998-08-29 04:41:47 +0000

    - Replace this with a version not containing 1.1 illegal Java

--- libraries/javalib/kaffe/util/Assert.java
+++ libraries/javalib/kaffe/util/Assert.java
@@ -10,16 +10,122 @@
 
 package kaffe.util;
 
+/**
+ * This class consists of static methods that perform assertion checks.
+ * If a check fails, we throw an error with a descriptive message.
+ *
+ * @author      Archie L. Cobbs <address>archie@whistle.com</address>
+ * @version     $Id: Assert.java,v 1.2 1998/08/29 04:41:47 archie Exp $
+ */
 public class Assert {
+  private static final String MSG = "assertion failure";
 
-  static public void true(boolean test, String report)
-  {
-    if (test == false) {
-      throw new Error("Failed assertion: " + report);
+  /**
+   * Throw an error if not <code>true</code>.
+   *
+   * @param	x	The value to test
+   */
+  public static void that(boolean x) {
+    if (!x) {
+      fail();
     }
   }
 
-  static public void false(boolean test, String report) {
-	Assert.true(!test, report);
+  /**
+   * Throw an error if not <code>true</code>.
+   *
+   * @param	x	The value to test
+   * @param	msg	Description of the failure
+   */
+  public static void that(boolean x, String msg) {
+    if (!x) {
+      fail(msg);
+    }
+  }
+
+  /**
+   * Throw an error if equal to zero.
+   *
+   * @param	x	The value to test
+   */
+  public static void that(long x) {
+    if (x == 0) {
+      fail();
+    }
+  }
+
+  /**
+   * Throw an error if equal to zero.
+   *
+   * @param	x	The value to test
+   * @param	msg	Description of the failure
+   */
+  public static void that(long x, String msg) {
+    if (x == 0) {
+      fail(msg);
+    }
+  }
+
+  /**
+   * Throw an error if equal to zero.
+   *
+   * @param	x	The value to test
+   */
+  public static void that(double x) {
+    if (x == 0.0) {
+      fail();
+    }
+  }
+
+  /**
+   * Throw an error if equal to zero.
+   *
+   * @param	x	The value to test
+   * @param	msg	Description of the failure
+   */
+  public static void that(double x, String msg) {
+    if (x == 0.0) {
+      fail(msg);
+    }
+  }
+
+  /**
+   * Throw an error if equal to <code>null</code>.
+   *
+   * @param	x	The value to test
+   */
+  public static void that(Object x) {
+    if (x == null) {
+      fail();
+    }
+  }
+
+  /**
+   * Throw an error if equal to <code>null</code>.
+   *
+   * @param	x	The value to test
+   * @param	msg	Description of the failure
+   */
+  public static void that(Object x, String msg) {
+    if (x == null) {
+      fail(msg);
+    }
+  }
+
+  /**
+   * Abort execution due to some failure.
+   */
+  public static void fail() {
+    throw new Error(MSG);
+  }
+
+  /**
+   * Abort execution due to some failure.
+   *
+   * @param	msg	Description of the failure
+   */
+  public static void fail(String msg) {
+    throw new Error(msg == null ? MSG : MSG + ": " + msg);
   }
 }
+
