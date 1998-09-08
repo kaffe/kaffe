35abe8368649ee878988eb62b8c5821addc189ef
tim <tim>
1998-09-08 23:43:27 +0000

    * Added copyright messages which accidentally got deleted.

--- libraries/javalib/java/text/SimpleDateFormat.java
+++ libraries/javalib/java/text/SimpleDateFormat.java
@@ -1,3 +1,13 @@
+/*
+ * Java core library component.
+ *    
+ * Copyright (c) 1997, 1998
+ *      Transvirtual Technologies, Inc.  All rights reserved.
+ * 
+ * See the file "license.terms" for information on usage and redistribution
+ * of this file.
+ */
+
 package java.text;
 
 import java.util.Date;
