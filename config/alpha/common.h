99262cf540e2ec5c263ea8d733720ffeea97050a
gback <gback>
1998-10-23 18:34:31 +0000

    Dirk Grunwald's alpha fixes

--- config/alpha/common.h
+++ config/alpha/common.h
@@ -26,16 +26,43 @@
  */
 #define	NO_KAFFE_STUBS
 
-#define getJavaArg(CALL, n)						      \
+/*
+ * For each argument, callsize[n] is either '1' or '2', saying
+ * how many words are being used.
+ */
+
+#define getSingleJavaArg(CALL, n)			      \
 (   /*									      \
      *  Sign-extend short arguments:					      \
      *									      \
      *  This macro sign extends short (i.e, 32-bit or less) arguments to a    \
      *  64-bit "long" that can be inserted directly into a 64-bit register.   \
      */									      \
-									      \
     ((CALL)->callsize[n] == 2) ? (CALL)->args[n].j : (long)((CALL)->args[n].i)\
 )
+
+
+#undef getSingleJavaArg
+#define getSingleJavaArg(CALL, n) (CALL) -> args[n].j
+
+/*									      \
+*  Sign-extend short arguments:					      \
+*									      \
+*  This macro sign extends short (i.e, 32-bit or less) arguments to a    \
+*  64-bit "long" that can be inserted directly into a 64-bit register.   \
+*/									      \
+#define getJavaArg(CALL, n, iarg, farg) \
+{ \
+    if ( (CALL) -> callsize[n] == 1 ) { \
+      iarg = (long)   ((CALL) -> args[n].i); \
+      farg = (double) ((CALL) ->  args[n].f); \
+    } else { \
+      iarg = (long)   ((CALL) -> args[n].j); \
+      farg = (double) ((CALL) -> args[n].d); \
+    } \
+}
+
+#define javaArgCase(CALL,_x_,_y_) case _x_: getJavaArg(CALL, (_x_-1), r ## _y_, f ## _y_)
 
 #define	sysdepCallMethod(CALL)						      \
 {   /*									      \
@@ -46,9 +73,34 @@
      *  return value in the info struct as well.			      \
      */									      \
 									      \
+    asm ("" ::: "$9", "$10", "$11"); \
+    asm ("" ::: "$16", "$17", "$18", "$19", "$20", "$21", "$9", "$27", "$26"); \
+    {                                                                         \
     int x, rags = 0;		    					      \
-    long lags[MAXMARGS-6];					      	      \
+    /* The parameter registers ... 					    */\
+    register long r16 asm("$16");					      \
+    register long r17 asm("$17");					      \
+    register long r18 asm("$18");					      \
+    register long r19 asm("$19");					      \
+    register long r20 asm("$20");					      \
+    register long r21 asm("$21");					      \
 									      \
+    register double f16 asm("$f16");					      \
+    register double f17 asm("$f17");					      \
+    register double f18 asm("$f18");					      \
+    register double f19 asm("$f19");					      \
+    register double f20 asm("$f20");					      \
+    register double f21 asm("$f21");					      \
+									      \
+    register unsigned long *osp asm("$9"); \
+    register unsigned long *ogp asm("$10"); \
+    /* any temporary register would do.. */  \
+    register unsigned long *nsp asm("$11"); \
+    register unsigned long* pv asm("$27"); \
+    asm ("mov	$30,%0" : "=r" (osp)); \
+    asm ("mov	$29,%0" : "=r" (ogp)); \
+    nsp = osp;						      \
+    pv = (CALL) -> function;				      \
     for (x = 0; x < (CALL)->nrargs; x++)				      \
     {   /*								      \
          *  The alpha has 64-bit registers, so we don't really need the ex-   \
@@ -63,44 +115,62 @@
         }								      \
     }									      \
 									      \
+    {                                                                         \
+									      \
     switch (rags)							      \
     {                                                                         \
-	/* The parameter registers ... 					    */\
-        register long r16 asm("$16");					      \
-        register long r17 asm("$17");					      \
-        register long r18 asm("$18");					      \
-        register long r19 asm("$19");					      \
-        register long r20 asm("$20");					      \
-        register long r21 asm("$21");					      \
-									      \
         default:							      \
-        {   /*								      \
-             *  If we have more than 6 arguments, we need to push them onto   \
-             *  the stack.  We've already made room for them by allocating    \
-             *  the "lags" array, so all we do here is copy them into it.     \
-             */								      \
-									      \
-            register long *lap;						      \
-            asm("bis $30,$30,%0" : "=r" (lap));                               \
-            for (x = 5; ++x <= rags; *lap++ = getJavaArg(CALL, x));           \
+	  {									\
+        /*		                                                              \
+         * We're doing to hack the frame buffer the difficult way..               \
+         */                                                                       \
+        int margs = rags - 6;        \
+        nsp = osp - margs;  \
+	/* Copy arguments to the stack we allocated */			      \
+	  for (x = 0; x < margs; x++) {						\
+	    nsp[x] = getSingleJavaArg(CALL, x+6);				\
+	  }									\
         }								      \
 									      \
         /* The first 6 arguments are loaded into the parameter registers.   */\
-        case 6: r21 = getJavaArg(CALL, 5);				      \
-        case 5: r20 = getJavaArg(CALL, 4);				      \
-        case 4: r19 = getJavaArg(CALL, 3);				      \
-        case 3: r18 = getJavaArg(CALL, 2);				      \
-        case 2: r17 = getJavaArg(CALL, 1);				      \
-        case 1: r16 = getJavaArg(CALL, 0);				      \
+        javaArgCase(CALL,6,21); \
+        javaArgCase(CALL,5,20); \
+        javaArgCase(CALL,4,19); \
+        javaArgCase(CALL,3,18); \
+        javaArgCase(CALL,2,17); \
+        javaArgCase(CALL,1,16); \
+        case 0:  /* nothing */ \
     }									      \
-									      \
-    asm("ldq $27,%0" :: "m" ((CALL)->function));			      \
-    asm("jsr $26,($27),0");						      \
-    asm("ldgp $29,0($26)");						      \
-    asm("stq $0,%0" : "=m" ((CALL)->ret->j));				      \
+    asm volatile ("mov %0,$30" :: "r" (nsp), \
+	/* include all the argument registers least they get DCE'd away */   \
+	"r" (pv), \
+	"r" (r16), "r" (r17), "r" (r18), "r" (r19), "r" (r20), "r" (r21) ); \
+    asm volatile ("\
+         jsr $26,($27),0 \n\
+         ldgp $29,0($26)\n\
+	 mov %0, $30" \
+	 :: "r" (osp), \
+	"f" (f16), "f" (f17), "f" (f18), "f" (f19), "f" (f20), "f" (f21) \
+	 /* and mention that we kill these.. */   \
+	 : \
+		 "$1", "$2", "$3", "$4", "$5", "$6", "$7", "$8", \
+		 "$22", "$23", "$24", "$25", "$27", "$26", \
+		 "$f0", "$f1", \
+	          /*  we don't modify the FP callee-saves registers */ \
+	 	  /* "$f2", "$f3", "$f4", "$f5", "$f6", "$f7", "$f8", "$f9", */ \
+		 "$f10", "$f11", "$f12", "$f13", "$f14", "$f15", \
+                 "$f16", "$f17", "$f18", "$f19",  "$f20", "$f21", \
+                 "$f22", "$f23", "$f24", "$f25", "$f26", "$f27", "$f28", "$f29", "$f30", \
+		 "memory"); \
+    }									      \
+    asm ("stq $0,%0" : "=m" ((CALL)->ret->j));				      \
 									      \
     /* Store floating point return value over (default) integer value ...   */\
-    if ((CALL)->rettype == 'D') asm("stt $f0,%0" : "=m" ((CALL)->ret->d));    \
-    if ((CALL)->rettype == 'F') asm("sts $f0,%0" : "=m" ((CALL)->ret->f));    \
+    {									      \
+      char rt = (CALL) -> rettype;									      \
+      if (rt == 'D') asm volatile ("stt $f0,%0" : "=m" ((CALL)->ret->d));    \
+      if (rt == 'F') asm volatile ("sts $f0,%0" : "=m" ((CALL)->ret->f));    \
+    }									      \
+    }									      \
 }
 #endif
--- test/regression/TestNative.java
+++ test/regression/TestNative.java
@@ -8,76 +8,130 @@
  */
 public class TestNative
 {
-    public static native int test16int(int a, int b, int c, int d,
+  public static native int test16int(int a, int b, int c, int d,
 				     int e, int f, int g, int h,
 				     int i, int j, int k, int l,
 				     int m, int n, int o, int p);
 
-    public static native long test16long(long a, long b, long c, long d,
-				     long e, long f, long g, long h,
-				     long i, long j, long k, long l,
-				     long m, long n, long o, long p);
+  public static native long test16long(long a, long b, long c, long d,
+				       long e, long f, long g, long h,
+				       long i, long j, long k, long l,
+				       long m, long n, long o, long p);
 
-    public static native long test16intlong(int a, long b, long c, long d,
-				     long e, long f, long g, long h,
-				     long i, int j, int k, long l,
-				     long m, long n, long o, long p);
+  public static native long test16intlong(int a, long b, long c, long d,
+					  long e, long f, long g, long h,
+					  long i, int j, int k, long l,
+					  long m, long n, long o, long p);
 
-    public static native float test16float(float a, float b, float c, float d,
-				     float e, float f, float g, float h,
-				     float i, float j, float k, float l,
-				     float m, float n, float o, float p);
+  public static native float test16float(float a, float b, float c, float d,
+					 float e, float f, float g, float h,
+					 float i, float j, float k, float l,
+					 float m, float n, float o, float p);
 
-    public static native double test16double(
-				double a, double b, double c, double d,
-				double e, double f, double g, double h,
-				double i, double j, double k, double l,
-				double m, double n, double o, double p);
+  public static native double test16double(
+				   double a, double b, double c, double d,
+				   double e, double f, double g, double h,
+				   double i, double j, double k, double l,
+				   double m, double n, double o, double p);
 
-    public static native double test16floatdouble(
+  public static native double test16floatdouble(
 				float a, double b, double c, double d,
 				double e, double f, double g, double h,
 				double i, float j, float k, double l,
 				double m, double n, double o, double p);
 
-    public static void main(String av[])
-    {
-if (true) {
-	int i = test16int(0x11111111, 0x22222222, 0x33333333, 0x44444444,
-			0x55555555, 0x66666666, 0x77777777, 0x88888888,
-			0x99999999, 0xAAAAAAAA, 0xBBBBBBBB, 0xCCCCCCCC,
-			0xDDDDDDDD, 0xEEEEEEEE, 0xFFFFFFFF, 0x12345678
-			);
-	System.out.println("test16int returned " + i);
-
-	long l = test16long(0x11111111, 0x22222222, 0x33333333, 0x44444444,
-			0x55555555, 0x66666666, 0x77777777, 0x88888888,
-			0x99999999, 0xAAAAAAAA, 0xBBBBBBBB, 0xCCCCCCCC,
-			0xDDDDDDDD, 0xEEEEEEEE, 0xFFFFFFFF, 0x12345678
-			);
-	System.out.println("test16long returned " + l);
-
-	     l = test16intlong(0x11111111, 0x22222222, 0x33333333, 0x44444444,
-			0x55555555, 0x66666666, 0x77777777, 0x88888888,
-			0x99999999, 0xAAAAAAAA, 0xBBBBBBBB, 0xCCCCCCCC,
-			0xDDDDDDDD, 0xEEEEEEEE, 0xFFFFFFFF, 0x12345678
-			);
-	System.out.println("test16intlong returned " + l);
-}
-if (true) {
-	float f = test16float(
-	  -1.2f, 0.2f, 3.3f, 4.5f, 5.6f, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
-			);
-	System.out.println("test16float returned " + f);
-}
-	double d = test16double(
-	  -1.2, 0.19, 3.3, 4.5, 5.6, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
-			);
-	System.out.println("test16double returned " + d);
-
-	d = test16floatdouble(
-	  -1.2f, 0.2, 3.3, 4.5, 5.6, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
-			);
-	System.out.println("test16floatdouble returned " + d);
+  public static double fabs(double x)
+  {
+    if ( x < 0 ) {
+      return -x;
+    } else {
+      return x;
     }
+  }
+
+  public static void main(String av[])
+  {
+    int i_should_be = 0x11111111 + 0x22222222 + 0x33333333 + 0x44444444 +
+      0x55555555 + 0x66666666 + 0x77777777 + 0x88888888 +
+      0x99999999 + 0xAAAAAAAA + 0xBBBBBBBB + 0xCCCCCCCC +
+      0xDDDDDDDD + 0xEEEEEEEE + 0xFFFFFFFF + 0x12345678;
+    System.out.println("Following test results should be " + i_should_be);
+
+    int i = test16int(0x11111111, 0x22222222, 0x33333333, 0x44444444,
+		      0x55555555, 0x66666666, 0x77777777, 0x88888888,
+		      0x99999999, 0xAAAAAAAA, 0xBBBBBBBB, 0xCCCCCCCC,
+		      0xDDDDDDDD, 0xEEEEEEEE, 0xFFFFFFFF, 0x12345678
+		      );
+    System.out.print("test16int returned " + i);
+    if ( i == i_should_be ) {
+      System.out.println("...SUCCESS");
+    } else {
+      System.out.println("...FAILURE");
+    }
+
+
+    long l_should_be = 0x11111111 + 0x22222222 + 0x33333333 + 0x44444444 +
+      0x55555555 + 0x66666666 + 0x77777777 + 0x88888888 +
+      0x99999999 + 0xAAAAAAAA + 0xBBBBBBBB + 0xCCCCCCCC +
+      0xDDDDDDDD + 0xEEEEEEEE + 0xFFFFFFFF + 0x12345678;
+    System.out.println("Following test results should be " + l_should_be);
+
+    long l = test16long(0x11111111, 0x22222222, 0x33333333, 0x44444444,
+			0x55555555, 0x66666666, 0x77777777, 0x88888888,
+			0x99999999, 0xAAAAAAAA, 0xBBBBBBBB, 0xCCCCCCCC,
+			0xDDDDDDDD, 0xEEEEEEEE, 0xFFFFFFFF, 0x12345678
+			);
+    System.out.print("test16long returned " + l);
+    if ( l == l_should_be ) {
+      System.out.println("...SUCCESS");
+    } else {
+      System.out.println("...FAILURE");
+    }
+
+    long l2_should_be = 1 +(-1) + 2 + (-2) + 3 + (-3) + 4 + (-4) +
+      11 +(-11) + 22 + (-22) + 33 + (-33) + 44 + (-44);
+
+    l = test16intlong(1 ,(-1) , 2 , (-2) , 3 , (-3) , 4 , (-4) ,
+      11 ,(-11) , 22 , (-22) , 33 , (-33) , 44 , (-44));
+
+    System.out.print("test16intlong returned " + l);
+    if ( l == l2_should_be ) {
+      System.out.println("...SUCCESS");
+    } else {
+      System.out.println("...FAILURE");
+    }
+
+    float f_should_be =  -1.2f + 0.2f + 3.3f + 4.5f + 5.6f + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16;
+    System.out.println("Following test results should be " + f_should_be);
+
+    float f = test16float(-1.2f, 0.2f, 3.3f, 4.5f, 5.6f, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 );
+    System.out.print("test16float returned " + f);
+    if ( f == f_should_be ) {
+      System.out.println("...SUCCESS");
+    } else {
+      System.out.println("...FAILURE");
+    }
+
+    double d_should_be =  -1.2 + 0.2 + 3.3 + 4.5 + 5.6 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16;
+    System.out.println("Following test results should be " + d_should_be);
+
+    double d = test16double(-1.2, 0.2, 3.3, 4.5, 5.6, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+    System.out.print("test16double returned " + d);
+    if ( d == d_should_be ) {
+      System.out.println("...SUCCESS");
+    } else {
+      System.out.println("...FAILURE");
+    }
+
+    d = test16floatdouble(-1.2f, 0.2, 3.3, 4.5,
+			  5.6, 6, 7, 8,
+			  9, 10f, 11f, 12,
+			  13, 14, 15, 16);
+    System.out.print("test16floatdouble returned " + d);
+    if ( fabs(d - d_should_be) < 0.001 ) {
+      System.out.println("...SUCCESS");
+    } else {
+      System.out.println("...FAILURE");
+    }
+  }
 }
