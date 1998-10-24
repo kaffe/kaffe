7d57c1199c6f4da88888e522414b786346ba44ef
tim <tim>
1998-10-24 00:00:38 +0000

    * Updated GridBags to work closer to the spec.

--- libraries/javalib/java/awt/GridBagConstraints.java
+++ libraries/javalib/java/awt/GridBagConstraints.java
@@ -1,6 +1,3 @@
-package java.awt;
-
-
 /**
  * class GridBagConstraints - 
  *
@@ -12,35 +9,43 @@ package java.awt;
  *
  * @author J.Mehlitz
  */
+
+package java.awt;
+
+import java.io.Serializable;
+
 public class GridBagConstraints
-  implements Cloneable
+  implements Cloneable, Serializable
 {
-	final public static int RELATIVE = -1;
-	final public static int REMAINDER = 0;
-	final public static int NONE = 0;
-	final public static int HORIZONTAL = 1;
-	final public static int VERTICAL = 2;
-	final public static int BOTH = 3;
-	final public static int NORTHWEST = 0x00;
-	final public static int NORTH = 0x01;
-	final public static int NORTHEAST = 0x02;
-	final public static int WEST = 0x10;
-	final public static int CENTER = 0x11;
-	final public static int EAST = 0x12;
-	final public static int SOUTHWEST = 0x20;
-	final public static int SOUTH = 0x21;
-	final public static int SOUTHEAST = 0x22;
-	public int gridx = RELATIVE;
-	public int gridy = RELATIVE;
-	public int gridwidth = 1;
-	public int gridheight = 1;
-	public double weightx = 0;
-	public double weighty = 0;
-	public int anchor = CENTER;
-	public int fill = NONE;
-	public int ipadx = 0;
-	public int ipady = 0;
-	public Insets insets = new Insets( 0, 0, 0, 0);
+
+final public static int RELATIVE = -1;
+final public static int REMAINDER = 0;
+final public static int NONE = 0;
+final public static int BOTH = 1;
+final public static int HORIZONTAL = 2;
+final public static int VERTICAL = 3;
+
+final public static int CENTER = 10;
+final public static int NORTH = 11;
+final public static int NORTHEAST = 12;
+final public static int EAST = 13;
+final public static int SOUTHEAST = 14;
+final public static int SOUTH = 15;
+final public static int SOUTHWEST = 16;
+final public static int WEST = 17;
+final public static int NORTHWEST = 18;
+
+public int gridx = RELATIVE;
+public int gridy = RELATIVE;
+public int gridwidth = 1;
+public int gridheight = 1;
+public double weightx = 0;
+public double weighty = 0;
+public int anchor = CENTER;
+public int fill = NONE;
+public int ipadx = 0;
+public int ipady = 0;
+public Insets insets = new Insets( 0, 0, 0, 0);
 
 public GridBagConstraints() {
 }
--- libraries/javalib/java/awt/GridBagLayout.java
+++ libraries/javalib/java/awt/GridBagLayout.java
@@ -1,7 +1,3 @@
-package java.awt;
-
-import java.util.Hashtable;
-
 /**
  * class GridBagLayout - 
  *
@@ -13,26 +9,43 @@ import java.util.Hashtable;
  *
  * @author J.Mehlitz
  */
+
+package java.awt;
+
+import java.util.Hashtable;
+
 public class GridBagLayout
   implements LayoutManager2
 {
-	final protected static int MINSIZE = 1;
-	final protected static int PREFERREDSIZE = 2;
-	final protected static int ACTUALSIZE = 3;
-	protected Hashtable comptable = new Hashtable();
-	protected GridBagConstraints defaultConstraints = new GridBagConstraints();
-	public int[] columnWidths;
-	public int[] rowHeights;
-	public double[] columnWeights;
-	public double[] rowWeights;
-	Point offset = new Point();
+
+final protected static int MINSIZE = 1;
+final protected static int PREFERREDSIZE = 2;
+final protected static int ACTUALSIZE = 3;
+final protected static int MAXGRIDSIZE = 512;
+
+protected Hashtable comptable = new Hashtable();
+protected GridBagConstraints defaultConstraints = new GridBagConstraints();
+//protected GridBagLayoutInfo layoutInfo;
+
+public int[] columnWidths;
+public int[] rowHeights;
+public double[] columnWeights;
+public double[] rowWeights;
+
+Point offset = new Point();
 
 public GridBagLayout() {
 }
 
 public void addLayoutComponent( Component c, Object constraints) {
-	if ( constraints instanceof GridBagConstraints)
-		comptable.put( c, ((GridBagConstraints)constraints).clone());
+	try {
+		if (constraints != null) {
+			setConstraints(c, (GridBagConstraints)constraints);
+		}
+	}
+	catch (ClassCastException _) {
+		throw new IllegalArgumentException("not GridBagConstraints");
+	}
 }
 
 public void addLayoutComponent( String name, Component c) {
@@ -80,8 +93,9 @@ void adjustWeightsRelative( Container parent) {
 			columnWidths[i] += dx;
 		offset.x = 0;
 	}
-	else
+	else {
 		offset.x = (parent.width - sw - piw) / 2;
+	}
 	
 	if ( nyw > 0) {
 		int dy = parent.height - sh - pih;
@@ -100,8 +114,9 @@ void adjustWeightsRelative( Container parent) {
 			rowHeights[i] += dy;
 		offset.y = 0;
 	}
-	else
+	else {
 		offset.y = (parent.height - sh - pih) / 2;
+	}
 
 }
 
@@ -114,20 +129,24 @@ int columnStart( int idx) {
 
 void dumpDims() {
 	System.out.print( "\t");
-	for ( int i=0; i<columnWidths.length; i++)
+	for ( int i=0; i<columnWidths.length; i++) {
 		System.out.print( columnWidths[i] + " ");
+	}
 	System.out.println();
-	for ( int i=0; i<rowHeights.length; i++)
+	for ( int i=0; i<rowHeights.length; i++) {
 		System.out.println( rowHeights[i] );
+	}
 }
 
 void dumpWeights() {
 	System.out.print( "\t");
-	for ( int i=0; i<columnWeights.length; i++)
+	for ( int i=0; i<columnWeights.length; i++) {
 		System.out.print( columnWeights[i] + " ");
+	}
 	System.out.println();
-	for ( int i=0; i<rowWeights.length; i++)
+	for ( int i=0; i<rowWeights.length; i++) {
 		System.out.println( rowWeights[i] );
+	}
 }
 
 void getCellDims( Container parent, int mode) {
@@ -137,55 +156,90 @@ void getCellDims( Container parent, int mode) {
 	int y = 0;
 	int nc = parent.getComponentCount();
 
-	if ( (rowHeights == null) || (rowHeights.length != d.height) )
+/*
+	if ( (rowHeights == null) || (rowHeights.length != d.height) ) {
 		rowHeights   = new int[d.height];
-	if ( (columnWidths == null) || (columnWidths.length != d.width) )
+	}
+	if ( (columnWidths == null) || (columnWidths.length != d.width) ) {
 		columnWidths = new int[d.width];
-	if ( (columnWeights == null) || (columnWeights.length != d.width) )
+	}
+	if ( (columnWeights == null) || (columnWeights.length != d.width) ) {
 		columnWeights = new double[d.width];
-	if ( (rowWeights == null) || (rowWeights.length != d.height) )
+	}
+	if ( (rowWeights == null) || (rowWeights.length != d.height) ) {
 		rowWeights = new double[d.height];
-	
+	}
+*/
+	rowHeights = new int[d.height];
+	columnWidths = new int[d.width];
+	columnWeights = new double[d.width];
+	rowWeights = new double[d.height];
+
 	for ( int ic=0; ic<nc; ic++){
 		Component c = parent.getComponent(ic);
 		GridBagConstraints cc = lookupConstraints( c);
-		if ( cc == null)
-			continue;
-		if ( mode == MINSIZE )
+		if ( mode == MINSIZE ) {
 			cd = c.getMinimumSize();
-		else
+		}
+		else {
 			cd = c.getPreferredSize();
+		}
 
-		if ( cc.gridx == cc.RELATIVE )
+		if ( cc.gridx == cc.RELATIVE ) {
 			x++;
-		else
+		}
+		else {
 			x = cc.gridx;
+		}
 		
-		if ( cc.gridy > cc.RELATIVE )
+		if ( cc.gridy > cc.RELATIVE ) {
 			y = cc.gridy;
+		}
 
 		int cw = cd.width + cc.insets.left + cc.insets.right + 2 * cc.ipadx;
 		int ch = cd.height + cc.insets.top + cc.insets.bottom + 2 * cc.ipady;
 		
-		int gw = Math.max( cc.gridwidth, 1);
-		int gh = Math.max( cc.gridheight, 1);
+		int gw = cc.gridwidth;
+		int gh = cc.gridheight;
+		if (gw == cc.REMAINDER) {
+			gw = d.width - x;
+		}
+		else if (gw == cc.RELATIVE) {
+			gw = d.width - x - 1;
+		}
+		if (gh == cc.REMAINDER) {
+			gh = d.height - y;
+		}
+		else if (gh == cc.RELATIVE) {
+			gh = d.height - y - 1;
+		}
+		if (gw < 1) {
+			gw = 1;
+		}
+		if (gh < 1) {
+			gh = 1;
+		}
+
 		cw = cw / gw;
 		ch = ch / gh;
 
 		for ( int i=0; i<gw; i++) {
-			if ( columnWidths[x+i] < cw )
+			if ( columnWidths[x+i] < cw ) {
 				columnWidths[x+i] = cw;
+			}
 		}
-		if ( columnWeights[x+gw-1] < cc.weightx )
+		if ( columnWeights[x+gw-1] < cc.weightx ) {
 			columnWeights[x+gw-1] = cc.weightx;
+		}
 
 		for ( int i=0; i<gh; i++) {
-			if ( rowHeights[y+i] < ch )
+			if ( rowHeights[y+i] < ch ) {
 				rowHeights[y+i] = ch;
+			}
 		}
-		if ( rowWeights[y+gh-1] < cc.weighty )
+		if ( rowWeights[y+gh-1] < cc.weighty ) {
 			rowWeights[y+gh-1] = cc.weighty;
-
+		}
 		
 		x += gw - 1;
 		if ( cc.gridwidth == cc.REMAINDER ){
@@ -194,31 +248,25 @@ void getCellDims( Container parent, int mode) {
 		}
 	}
 	
-	if ( mode == ACTUALSIZE)
+	if ( mode == ACTUALSIZE) {
 		adjustWeightsRelative( parent);
+	}
 }
 
 public GridBagConstraints getConstraints( Component c) {
-	GridBagConstraints gc = defaultConstraints;
-	if ( comptable.containsKey( c) )
-		gc = (GridBagConstraints)comptable.get( c);
-		
-	return (GridBagConstraints)gc.clone();
+	return ((GridBagConstraints)lookupConstraints(c).clone());
 }
 
 public float getLayoutAlignmentX( Container parent) {
-	return (float)0.5;
+	return (float)Component.CENTER_ALIGNMENT;
 }
 
 public float getLayoutAlignmentY( Container parent) {
-	return (float)0.5;
+	return (float)Component.CENTER_ALIGNMENT;
 }
 
 public int[][] getLayoutDimensions() {
-	int ld[][] = new int[2][];
-	ld[0] = columnWidths;
-	ld[1] = rowHeights;
-	return ld;
+	return (new int[][]{columnWidths, rowHeights});
 }
 
 Dimension getLayoutExtends( Container parent ) {
@@ -231,46 +279,52 @@ Dimension getLayoutExtends( Container parent ) {
 	for ( int i=0; i<nc; i++){
 		Component c = parent.getComponent(i);
 		GridBagConstraints cc = lookupConstraints( c);
-		if ( cc == null )
-			continue;
-		if ( cc.gridx >= 0)
+		if ( cc.gridx >= 0) {
 			x = cc.gridx;
-		else
+		}
+		else {
 			x++;
-		if ( cc.gridy >= 0)
+		}
+		if ( cc.gridy >= 0) {
 			y = cc.gridy;
-
-		switch( cc.gridwidth) {
-			case cc.REMAINDER:	//last in row
-				if ( maxX < x+1 )
-					maxX = x+1;
-				x = -1;
-				y++;
-				break;
-			case cc.RELATIVE:
-				if ( maxX < x+1 )
-					maxX = x+1;
-				break;			  
-			default:
-				x += cc.gridwidth - 1;
-				if ( maxX < x+1 )
-					maxX = x+1;
-				break;
-		}
-		switch( cc.gridheight) {
-			case cc.RELATIVE:
-				break;
-			case cc.REMAINDER:
-				break;
-			default:
-				if ( x != -1 )
-					maxY = Math.max( maxY, y+cc.gridheight);
-				break;
 		}
 
+		switch(cc.gridwidth) {
+		case cc.REMAINDER:	//last in row
+			if ( maxX < x ) {
+				maxX = x;
+			}
+			x = -1;
+			y++;
+			break;
+
+		case cc.RELATIVE:
+			if ( maxX < x ) {
+				maxX = x;
+			}
+			break;			  
+
+		default:
+			x += cc.gridwidth - 1;
+			if ( maxX < x ) {
+				maxX = x;
+			}
+			break;
+		}
+
+		switch(cc.gridheight) {
+		case cc.RELATIVE:
+		case cc.REMAINDER:
+			break;
+
+		default:
+			if (x != -1 && maxY < y + cc.gridheight) {
+				maxY = y + cc.gridheight;
+			}
+			break;
+		}
 	}
-	maxY = Math.max( maxY, y);
-	return new Dimension( maxX, maxY);
+	return new Dimension(maxX + 1, Math.max(maxY, y));
 }
 
 public Point getLayoutOrigin() {
@@ -278,14 +332,11 @@ public Point getLayoutOrigin() {
 }
 
 public double[][] getLayoutWeights() {
-	double lw[][] = new double[2][];
-	lw[0] = columnWeights;
-	lw[1] = rowWeights;
-	return lw;
+	return (new double[][]{columnWeights, rowWeights});
 }
 
 public void invalidateLayout( Container parent) {
-layoutContainer( parent);
+	layoutContainer( parent);
 }
 
 public void layoutContainer( Container parent) {
@@ -297,13 +348,11 @@ public void layoutContainer( Container parent) {
 	Rectangle relRect = new Rectangle();
 	int nc = parent.getComponentCount();
 	
-	getCellDims( parent, ACTUALSIZE);
+	getCellDims(parent, ACTUALSIZE);
 
-	for ( int ci=0; ci<nc; ci++){
+	for ( int ci=0; ci < nc; ci++){
 		Component c = parent.getComponent(ci);
 		GridBagConstraints cc = lookupConstraints( c);
-		if ( cc == null )
-			continue;
 		boolean newRow = false;
 
 		if ( cc.gridy >= 0 ) {
@@ -312,8 +361,9 @@ public void layoutContainer( Container parent) {
 		}
 		if ( cc.gridx < 0 ) {
 			ix++;
-			if ( iy > relRect.y )
+			if ( iy > relRect.y ) {
 				ix += relRect.x + relRect.width;
+			}
 			x = columnStart( ix) + pin.left;
 		}
 		else {
@@ -340,69 +390,138 @@ public void layoutContainer( Container parent) {
 		}
 
 		dx = 0;
-		for ( i=0; i<gw; i++)
+		for ( i=0; i<gw; i++) {
 			dx += columnWidths[ix++];
+		}
 		ix--;
 		dy = 0;
-		for ( i=0; i<gh; i++)
+		for ( i=0; i<gh; i++) {
 			dy += rowHeights[iy+i]; 
+		}
 			
-		if ( gh > 1)
+		if ( gh > 1) {
 			relRect.setBounds( ix, iy, gw, gh);
-		else if ( iy >= relRect.y + relRect.height )
+		}
+		else if ( iy >= relRect.y + relRect.height ) {
 			relRect.setBounds( 0, 0, 0, 0);
+		}
 			
 		Insets in = cc.insets;
-		if ( cc.fill == cc.BOTH ) {
-			c.setBounds( x+in.left+cc.ipadx+offset.x,
-									 y+in.top+cc.ipady+offset.y,
-									 dx-in.left-in.right-2*cc.ipadx,
-									 dy-in.top-in.bottom-2*cc.ipady);
+		Dimension cd = c.getPreferredSize();
+		int cx = x + offset.x;
+		int cy = y + offset.y;
+		int cw = cd.width;
+		int ch = cd.height;
+
+		switch ( cc.fill ) {
+		case cc.BOTH:
+			cx += in.left + cc.ipadx;
+			cy += in.top + cc.ipady;
+			cw = dx - (in.left + in.right + 2 * cc.ipadx);
+			ch = dy - (in.top + in.bottom + 2 * cc.ipady);
+			break;
+
+		case cc.HORIZONTAL:
+			cx += in.left + cc.ipadx;
+			cw = dx - (in.left + in.right + 2 * cc.ipadx);
+			switch ( cc.anchor ) {
+			case cc.WEST:
+			case cc.CENTER:
+			case cc.EAST:
+			default:
+				cy += (dy - ch) / 2;
+				break;
+
+			case cc.NORTH:
+			case cc.NORTHEAST:
+			case cc.NORTHWEST:
+				cy += in.top + cc.ipady;
+				break;
+
+			case cc.SOUTHEAST:
+			case cc.SOUTH:
+			case cc.SOUTHWEST:
+				cy += dy - (cd.height + in.bottom + cc.ipady);
+				break;
+			}
+			break;
+
+		case cc.VERTICAL:
+			cy += in.top + cc.ipady;
+			cw = dy - (in.top + in.bottom + 2 * cc.ipady);
+			switch ( cc.anchor ) {
+			case cc.NORTH:
+			case cc.CENTER:
+			case cc.SOUTH:
+			default:
+				cx += (dx - cw) / 2;
+				break;
+
+			case cc.NORTHWEST:
+			case cc.WEST:
+			case cc.SOUTHWEST:
+				cx += in.left + cc.ipadx;
+				break;
+
+			case cc.NORTHEAST:
+			case cc.SOUTHEAST:
+			case cc.EAST:
+				cx += dx - (cw + in.right + cc.ipadx);
+				break;
+			}
+			break;
+
+		case cc.NONE:
+		default:
+			switch ( cc.anchor ) {
+			case cc.NORTH:
+				cy += in.top + cc.ipady;
+				cx += (dx - cw) / 2;
+				break;
+
+			case cc.NORTHEAST:
+				cy += in.top + cc.ipady;
+				cx += dx - (cw + in.right + cc.ipadx);
+				break;
+
+			case cc.EAST:
+				cy += (dy - ch) / 2;
+				cx += dx - (cw + in.right + cc.ipadx);
+				break;
+
+			case cc.SOUTHEAST:
+				cy += dy - (ch + in.bottom + cc.ipady);
+				cx += dx - (cw + in.right + cc.ipadx);
+				break;
+
+			case cc.SOUTH:
+				cy += dy - (ch + in.bottom + cc.ipady);
+				cx += (dx - cw) / 2;
+				break;
+
+			case cc.SOUTHWEST:
+				cy += dy - (ch + in.bottom + cc.ipady);
+				cx += in.left + cc.ipadx;
+				break;
+
+			case cc.WEST:
+				cy += (dy - ch) / 2;
+				cx += in.left + cc.ipadx;
+				break;
+
+			case cc.NORTHWEST:
+				cy += in.top + cc.ipady;
+				cx += in.left + cc.ipadx;
+				break;
+
+			case cc.CENTER:
+			default:
+				cy += (dy - ch) / 2;
+				cx += (dx - cw) / 2;
+				break;
+			}
 		}
-		else {
-			int cx, cy, cw, ch;
-			Dimension cd = c.getPreferredSize();
-			if ( cc.fill == cc.HORIZONTAL ){
-				cx = x+in.left+cc.ipadx+offset.x;
-				cw = dx-in.left-in.right-2*cc.ipadx;
-				ch = cd.height;
-				if ( (cc.anchor & cc.SOUTHWEST) > 0)
-					cy = y+dy-cd.height-cc.ipady-in.bottom+offset.y;
-				else if ( (cc.anchor & cc.WEST) > 0)
-					cy = y+(dy-ch)/2+offset.y;
-				else
-					cy = y+cc.ipady+in.top+offset.y;
-			}
-			else if ( cc.fill == cc.VERTICAL ) {
-				cy = y+in.top+cc.ipady+offset.y;
-				ch = dy-in.top-in.bottom-2*cc.ipady;
-				cw = cd.width;
-				if ( (cc.anchor & cc.NORTHEAST) > 0)
-					cx = x+dx-cw-cc.ipadx-in.right+offset.x;
-				else if ( (cc.anchor & cc.NORTH) > 0)
-					cx = x+(dx-cw)/2+offset.x;
-				else
-					cx = x+cc.ipadx+in.left+offset.x;
-			}
-			else {
-				cw = cd.width;
-				ch = cd.height;
-				if ( (cc.anchor & cc.SOUTHWEST) > 0)
-					cy = y+dy-cd.height-cc.ipady-in.bottom+offset.y;
-				else if ( (cc.anchor & cc.WEST) > 0)
-					cy = y+(dy-ch)/2+offset.y;
-				else
-					cy = y+cc.ipady+in.top+offset.y;
-				if ( (cc.anchor & cc.NORTHEAST) > 0)
-					cx = x+dx-cw-cc.ipadx-in.right+offset.x;
-				else if ( (cc.anchor & cc.NORTH) > 0)
-					cx = x+(dx-cw)/2+offset.x;
-				else
-					cx = x+cc.ipadx+in.left+offset.x;
-			}
-			
-			c.setBounds( cx, cy, cw, ch);
-		}
+		c.setBounds( cx, cy, cw, ch);
 
 		if ( newRow ) {
 			ix = -1;
@@ -417,29 +536,32 @@ public Point location( int x, int y) {
 	int y0 = offset.y;
 	int ix, iy;
 	
-	for ( ix=0; ix<columnWidths.length; ix++){
+	for ( ix = 0; ix < columnWidths.length; ix++){
 		x0 += columnWidths[ix];
-		if ( x < x0 )
+		if ( x < x0 ) {
 			break;
+		}
 	}
-	for ( iy=0; ix<rowHeights.length; iy++){
+	for ( iy = 0; ix < rowHeights.length; iy++){
 		y0 += rowHeights[iy];
-		if ( y < y0 )
+		if ( y < y0 ) {
 			break;
+		}
 	}
 	
 	return new Point( ix, iy);
 }
 
-protected GridBagConstraints lookupConstraints( Component c) {
-	if ( comptable.containsKey( c) )
-		return (GridBagConstraints)comptable.get( c);
-//	return defaultConstraints;
-	return null;
+protected GridBagConstraints lookupConstraints(Component c) {
+	GridBagConstraints cons = (GridBagConstraints)comptable.get(c);
+	if (cons == null) {
+		cons = defaultConstraints;
+	}
+	return (cons);
 }
 
 public Dimension maximumLayoutSize( Container parent) {
-	return Toolkit.singleton.getScreenSize();
+	return (new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
 }
 
 public Dimension minimumLayoutSize( Container parent) {
@@ -455,32 +577,34 @@ public Dimension preferredLayoutSize( Container parent) {
 }
 
 public void removeLayoutComponent( Component c) {
-	comptable.remove( c);
+	comptable.remove(c);
 }
 
 int rowStart( int idx) {
 	int h = 0;
-	for ( int i=0; i<idx; i++)
+	for ( int i=0; i<idx; i++) {
 		h += rowHeights[i];
+	}
 	return h;
 }
 
 public void setConstraints( Component comp, GridBagConstraints cons) {
-	GridBagConstraints gc = (GridBagConstraints)cons.clone();
-	comptable.put( comp, gc);
+	comptable.put(comp, (GridBagConstraints)cons.clone());
 }
 
 int sumHeights() {
 	int h = 0;
-	for ( int i=0; i<rowHeights.length; i++)
+	for ( int i=0; i<rowHeights.length; i++) {
 		h += rowHeights[i];
+	}
 	return h;
 }
 
 int sumWidths() {
 	int w = 0;
-	for ( int i=0; i<columnWidths.length; i++)
+	for ( int i=0; i<columnWidths.length; i++) {
 		w += columnWidths[i];
+	}
 	return w;
 }
 
@@ -489,4 +613,16 @@ public String toString() {
 	int h = (rowHeights != null) ? rowHeights.length : 0;
 	return ("GridBagLayout columns: " + w + ",rows: " + h);
 }
+
+protected void AdjustForGravity(GridBagConstraints cons, Rectangle display) {
+	throw new kaffe.util.NotImplemented();
+}
+
+protected void ArrangeGrid(Container container) {
+	layoutContainer(container);
+}
+
+//protected Dimensions GetMinSize(Container con, GridBagLayoutInfo info) {
+//}
+
 }
