20eac1e984d17cad1c1a4b0811d4f1fc39c205ee
archie <archie>
1998-12-04 04:22:42 +0000

    * libraries/javalib/java/io/BufferedReader.java: fix bug in        ready() method and do some rewriting/optimization in the process.

--- libraries/javalib/java/io/BufferedReader.java
+++ libraries/javalib/java/io/BufferedReader.java
@@ -17,8 +17,8 @@ public class BufferedReader
 	final private static int DEFAULTBUFFERSIZE = 1024;
 	private Reader rd;
 	private char[] inbuf;
-	private int pos;
-	private int size;
+	private int pos;	// position of next char in buffer
+	private int size;	// total length of valid chars in buffer
 	private boolean markset;
 
 public BufferedReader(Reader in) {
@@ -39,14 +39,16 @@ public void close() throws IOException {
 
 public void mark(int readAheadLimit) throws IOException {
 	synchronized(lock) {
-
 		char[] oldbuf = inbuf;
 
+		// Allocate bigger buffer if necessary
 		if (readAheadLimit > inbuf.length) {
 			inbuf = new char[readAheadLimit];
 		}
-		/* Copy data to the beginning of the buffer */
-		System.arraycopy(oldbuf, pos, inbuf, 0, size);
+
+		// Shift data to the beginning of the buffer
+		System.arraycopy(oldbuf, pos, inbuf, 0, size - pos);
+		size -= pos;
 		pos = 0;
 		markset = true;
 	}
@@ -57,53 +59,51 @@ public boolean markSupported() {
 }
 
 protected void pushback() {
-	size++;
 	pos--;
 }
 
 public int read () throws IOException {
 	synchronized ( lock ) {
-		if (pos < size) {
+		if (pos < size || refillBuffer() > 0) {
 			return (inbuf[pos++]);
 		}
-		else if (refillBuffer() > 0) {
-			return (inbuf[pos++]);
-		}
-		else {
-			return (-1);
-		}
+		return (-1);
 	}
 }
 
 public int read ( char cbuf[], int off, int len ) throws IOException {
-	int cnt;
-
 	synchronized(lock) {
-		for (;;) {
-			for (cnt = 0; cnt < size && cnt < len; cnt++) {
-				cbuf[off++] = inbuf[pos++];
-			}
-			size -= cnt;
-			len -= cnt;
 
-			/* If we're out of buffer space then return now */
-			if (len == 0) {
-				return (cnt);
-			}
-
-			// If we've put something in the buffer already, and
-			// the last refill was short, we terminate now.
-			if (cnt > 0 && size != inbuf.length) {
-				return (cnt);
-			}
-
-			if (refillBuffer() <= 0) {
-				if (cnt <= 0) {
-					return (-1);
-				}
-				return (cnt);
-			}
+		// Do read directly in this case, according to JDK 1.2 docs
+		if (pos == size && !markset && len >= inbuf.length) {
+			return rd.read(cbuf, off, len);
 		}
+
+		int nread, chunk;
+		for (nread = 0; nread < len; nread += chunk) {
+
+			// Make sure there's something in the buffer
+			if (pos == size) {
+				// Avoid unneccesary blocking
+				if (nread > 0 && !rd.ready()) {
+					return nread;
+				}
+				// Try to fill buffer
+				if (refillBuffer() <= 0) {
+					return (nread > 0) ? nread : -1;
+				}
+			}
+
+			// Copy next chunk of chars from buffer to output array
+			chunk = len - nread;
+			if (chunk > size) {
+				chunk = size;
+			}
+			System.arraycopy(inbuf, pos, cbuf, off, chunk);
+			pos += chunk;
+			off += chunk;
+		}
+		return (nread);
 	}
 }
 
@@ -114,7 +114,13 @@ public String readLine () throws IOException {
 
 	synchronized ( lock ) {
 		while ( true ) {
-			for (; pos < size && (c = inbuf[pos]) != '\n' && c != '\r'; pos++);
+
+			// Find next newline or carriage return
+			while (pos < size
+				&& (c = inbuf[pos]) != '\n' && c != '\r')
+			    pos++;
+
+			// Did we see one?
 			if (pos == size) {	// nothing found yet
 				if (pos > start) {
 					if (s == null) {
@@ -150,25 +156,18 @@ public String readLine () throws IOException {
 
 public boolean ready() throws IOException {
 	synchronized(lock) {
-
-		if (size > 0 || rd.ready()) {
-			return (true);
-		}
-		else {
-			return (false);
-		}
-
+		return (pos < size || rd.ready());
 	}
 }
 
 private int refillBuffer () throws IOException {
-	int n;
-
 	synchronized ( lock ) {
+		int n;
+
 		n = rd.read( inbuf, 0, inbuf.length);
 		pos = 0;
 		markset = false;
-			
+
 		if (n > 0) {
 			size = n;
 			return (n);
@@ -182,49 +181,34 @@ private int refillBuffer () throws IOException {
 
 public void reset() throws IOException {
 	synchronized(lock) {
-
 		if (!markset) {
 			throw new IOException("invalid mark");
 		}
-		/* Reset to the beginning of the buffer */
-		size += pos;
-		pos = 0;
-
+		pos = 0;	// reset to the beginning of the buffer
 	}
 }
 
 public long skip(long n) throws IOException {
-
 	synchronized(lock) {
+		long bufskip;
 
-		/* If we can skip without leaving the buffer do that now */
-		if (n < (long)size) {
-			size -= (int)n;
-			pos += (int)n;
-			return (n);
+		// Skip from within the buffer first
+		bufskip = size - pos;
+		if (bufskip > n) {
+			bufskip = n;
+		}
+		pos += (int)bufskip;		// cast is OK
+
+		// Anything more to skip?
+		if (bufskip == n) {
+			return n;
 		}
 
-		/* Otherwise empty the buffer and skip though the data stream */
-		long cnt = (long)size;
-		n -= cnt;
-		size = 0;
+		// Reset buffer and skip what remains directly
 		pos = 0;
-
-		for (;;) {
-			markset = false;
-			long r = (long)rd.read(inbuf, 0, inbuf.length);
-			if (r == -1) {
-				return (cnt);
-			}
-			if (r > n) {
-				pos = (int)n;
-				size = (int)(r - n);
-				cnt += n;
-				return (cnt);
-			}
-			cnt += r;
-			n -= r;
-		}
+		size = 0;
+		markset = false;
+		return bufskip + rd.skip(n - bufskip);
 	}
 }
 }
