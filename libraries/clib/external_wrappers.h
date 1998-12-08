825321dfd99e2590b87afd44ce1359fa0cea521e
tim <tim>
1998-12-08 05:55:21 +0000

    * Tidied copyrights.

--- libraries/clib/external_wrappers.h
+++ libraries/clib/external_wrappers.h
@@ -5,7 +5,7 @@
  * Copyright (c) 1996, 1997, 1998
  *	Transvirtual Technologies, Inc.  All rights reserved.
  *
- * See the file "lib-license.terms" for information on usage and redistribution 
+ * See the file "license.terms" for information on usage and redistribution 
  * of this file. 
  */
 
--- libraries/clib/native/Method.c
+++ libraries/clib/native/Method.c
@@ -4,7 +4,7 @@
  * Copyright (c) 1996, 1997
  *	Transvirtual Technologies, Inc.  All rights reserved.
  *
- * See the file "lib-license.terms" for information on usage and redistribution 
+ * See the file "license.terms" for information on usage and redistribution 
  * of this file. 
  */
 
