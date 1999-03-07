8ffb104c7d1bb993f0d7f4d7c068ac3344026ed8
archie <archie>
1999-03-07 03:05:09 +0000

    The semantics of a PrintStream and a PrintWriter are sufficiently different that it doesn't work to use a PrintWriter to implement a PrintStream... so we completely rewrite this class to behave according to the Java 2 spec.

--- libraries/javalib/java/io/PrintStream.java
+++ libraries/javalib/java/io/PrintStream.java
@@ -1,7 +1,5 @@
 package java.io;
 
-import java.lang.String;
-
 /*
  * Java core library component.
  *
@@ -11,128 +9,182 @@ import java.lang.String;
  * See the file "license.terms" for information on usage and redistribution
  * of this file.
  */
-public class PrintStream
-  extends FilterOutputStream
+public class PrintStream extends FilterOutputStream
 {
-	private PrintWriter strm;
-	private boolean error;
+	private static final char[] newline =
+			System.getProperty("line.separator").toCharArray();
+	private final boolean autoFlush;
+	private boolean error = false;
 
-/**
- * @deprecated - replaced by PrintWriter
- */
 public PrintStream(OutputStream out) {
 	this(out, false);
 }
 
-/**
- * @deprecated - replaced by PrintWriter
- */
-public PrintStream(OutputStream out, boolean autoflush) {
+public PrintStream(OutputStream out, boolean autoFlush) {
 	super(out);
-	strm = new PrintWriter(out, autoflush);
-	error = false;
+	this.autoFlush = autoFlush;
 }
 
 public boolean checkError() {
-	return (error || strm.checkError());
-}
-
-public void close() {
-	strm.close();
-}
-
-public void flush() {
-	strm.flush();
-}
-
-public void print(Object obj) {
-	strm.print(obj);
-}
-
-public void print(String s) {
-	strm.print(s);
-}
-
-public void print(boolean b) {
-	strm.print(b);
-}
-
-public void print(char c) {
-	strm.print(c);
-}
-
-public void print(char s[]) {
-	strm.print(s);
-}
-
-public void print(double d) {
-	strm.print(d);
-}
-
-public void print(float f) {
-	strm.print(f);
-}
-
-public void print(int i) {
-	strm.print(i);
-}
-
-public void print(long l) {
-	strm.print(l);
-}
-
-public void println() {
-	strm.println();
-}
-
-public void println(Object obj) {
-	strm.println(obj);
-}
-
-public void println(String s) {
-	strm.println(s);
-}
-
-public void println(boolean b) {
-	strm.println(b);
-}
-
-public void println(char c) {
-	strm.println(c);
-}
-
-public void println(char s[]) {
-	strm.println(s);
-}
-
-public void println(double d) {
-	strm.println(d);
-}
-
-public void println(float f) {
-	strm.println(f);
-}
-
-public void println(int i) {
-	strm.println(i);
-}
-
-public void println(long l) {
-	strm.println(l);
+	flush();
+	return error;
 }
 
 public void setError()
-	{
+{
 	error = true;
 }
 
-public void write(byte b[], int off, int len) {
-	for (int i = 0; i < len; i++) {
-		write((int)b[i+off]);
+public void close() {
+	try {
+		flush();
+		out.close();
+	}
+	catch (InterruptedIOException _) {
+		Thread.currentThread().interrupt();
+	}
+	catch (IOException _) {
+		error = true;
 	}
 }
 
-public void write(int b) {
-	strm.write(b);
+public void flush() {
+	try {
+		out.flush();
+	}
+	catch (InterruptedIOException _) {
+		Thread.currentThread().interrupt();
+	}
+	catch (IOException _) {
+		error = true;
+	}
 }
+
+public void write(int x) {
+	try {
+		out.write(x);
+	}
+	catch (InterruptedIOException _) {
+		Thread.currentThread().interrupt();
+	}
+	catch (IOException _) {
+		error = true;
+	}
+	if (x == '\n' && autoFlush)
+		flush();
+}
+
+public void write(byte b[], int off, int len) {
+	try {
+		out.write(b, off, len);
+	}
+	catch (InterruptedIOException _) {
+		Thread.currentThread().interrupt();
+	}
+	catch (IOException _) {
+		error = true;
+	}
+	if (autoFlush)
+		flush();
+}
+
+public void print(boolean x) {
+	print(String.valueOf(x).toCharArray());
+}
+
+public void print(char x) {
+	print(new char[] { x });
+}
+
+public void print(int x) {
+	print(String.valueOf(x).toCharArray());
+}
+
+public void print(long x) {
+	print(String.valueOf(x).toCharArray());
+}
+
+public void print(float x) {
+	print(String.valueOf(x).toCharArray());
+}
+
+public void print(double x) {
+	print(String.valueOf(x).toCharArray());
+}
+
+public void print(char x[]) {
+	for (int k = 0; k < x.length; k++)
+		write((int) x[k]);	// this is our default char encoding
+}
+
+public void print(String x) {
+	print((x != null ? x : "null").toCharArray());
+}
+
+public void print(Object x) {
+	print(String.valueOf(x).toCharArray());
+}
+
+public void println()
+{
+	print(newline);
+	if (autoFlush) {
+		flush();
+	}
+}
+
+public void println(boolean x)
+{
+	print(x);
+	println();
+}
+
+public void println(char x)
+{
+	print(x);
+	println();
+}
+
+public void println(int x)
+{
+	print(x);
+	println();
+}
+
+public void println(long x)
+{
+	print(x);
+	println();
+}
+
+public void println(float x)
+{
+	print(x);
+	println();
+}
+
+public void println(double x)
+{
+	print(x);
+	println();
+}
+
+public void println(char[] x)
+{
+	print(x);
+	println();
+}
+
+public void println(String x)
+{
+	print(x);
+	println();
+}
+
+public void println(Object x)
+{
+	print(x);
+	println();
+}
+
 }
