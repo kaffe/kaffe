9f3bfd76e6fd3cb5bf4d42470ae845bdc84d9ff1
oliva <oliva>
1999-03-12 20:44:33 +0000

    run autoheader with -l config, so that it uses config/acconfig.h

--- developers/autogen.sh
+++ developers/autogen.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#! /bin/sh
 
 # This script runs all of the various auto* programs in the correct order.
 # Written by Mo DeJong.
 
 aclocal -I .
-autoheader
+autoheader -l config
 automake --add-missing --verbose
 autoconf
 
