ccd67e636d28488dda03885ba0629c6512127dd2
archie <archie>
1999-06-02 02:31:03 +0000

    Add checks for indexOf() and lastIndexOf() with empty string arguments

--- test/regression/Str2.java
+++ test/regression/Str2.java
@@ -3,10 +3,17 @@ class Str2 {
 		String a = "1";
 		a += "2";
 		System.out.println(a);
+		System.out.println("abc".indexOf("", -32));
+		System.out.println("abc".indexOf("", 100));
+		System.out.println("abc".lastIndexOf("", -32));
+		System.out.println("abc".lastIndexOf("", 100));
 	}
 }
 
-
 /* Expected Output:
 12
+0
+-1
+-1
+3
 */
