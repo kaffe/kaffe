5bf2eaf303018cdb1863848167e1e1b34574f472
archie <archie>
1999-08-07 00:09:56 +0000

    Implement some unimplemented methods.

--- libraries/javalib/java/text/RuleBasedCollator.java
+++ libraries/javalib/java/text/RuleBasedCollator.java
@@ -10,56 +10,60 @@
 
 package java.text;
 
-import java.lang.String;
 import kaffe.util.NotImplemented;
 
 public class RuleBasedCollator extends Collator {
+	private final String rules;
 
-public RuleBasedCollator(String rules) throws ParseException {
-	throw new kaffe.util.NotImplemented(RuleBasedCollator.class.getName() + ".<init>(String)");
-}
-
-public Object clone() {
-	return (super.clone());
-}
-
-public int compare(String src, String target) {
-	/*
- 	 * XXX - this provide simple comparisons before we don't implement
-	 * this yet.
-	 */
-	return (src.compareTo(target));
-}
-
-public boolean equals(Object obj) {
-	// Do the simple stuff first.
-	if (obj == (Object)this) {
-		return (true);
+	public RuleBasedCollator(String rules) throws ParseException {
+		this.rules = rules;
 	}
-	if (!(obj instanceof RuleBasedCollator)) {
-		return (false);
+
+	public Object clone() {
+		return (super.clone());
 	}
-	RuleBasedCollator other = (RuleBasedCollator)obj;
 
-	// Now we have to do the hard stuff ...
+	public int compare(String src, String target) {
+		/*
+		 * XXX - this provide simple comparisons because
+		 * we don't implement this yet.
+		 */
+		return (src.compareTo(target));
+	}
 
-	return (false);
-}
+	public boolean equals(Object obj) {
+		// Do the simple stuff first.
+		if (obj == (Object)this) {
+			return (true);
+		}
+		if (!(obj instanceof RuleBasedCollator)) {
+			return (false);
+		}
+		RuleBasedCollator that = (RuleBasedCollator)obj;
 
-public CollationElementIterator getCollationElementIterator(String src) {
-	throw new kaffe.util.NotImplemented(RuleBasedCollator.class.getName() + ".getCollationElementIterator(String)");
-}
+		// Now we have to do the hard stuff ...
+		return this.rules.equals(that.rules);
+	}
 
-public CollationKey getCollationKey(String src) {
-	throw new kaffe.util.NotImplemented(RuleBasedCollator.class.getName() + ".getCollationKey(String)");
-}
+	public CollationElementIterator getCollationElementIterator(String s) {
+		return new CollationElementIterator(s);
+	}
 
-public String getRules() {
-	throw new kaffe.util.NotImplemented(RuleBasedCollator.class.getName() + ".getRules()");
-}
+	public CollationElementIterator getCollationElementIterator(
+			CharacterIterator source) {
+		return new CollationElementIterator(source);
+	}
 
-public int hashCode() {
-	return (System.identityHashCode(this));
-}
+	public CollationKey getCollationKey(String src) {
+		return new CollationKey(this, src);
+	}
+
+	public String getRules() {
+		return rules;
+	}
+
+	public int hashCode() {
+		return (System.identityHashCode(this));
+	}
 
 }
