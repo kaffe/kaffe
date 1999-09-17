96b3dc4e1dbcc002e9d69ff6fb451909387d05e4
archie <archie>
1999-09-17 23:27:40 +0000

    Adjust for new paramter to printStackTrace().

--- libraries/clib/native/Throwable.c
+++ libraries/clib/native/Throwable.c
@@ -19,7 +19,7 @@
 #include "../../../kaffe/kaffevm/support.h"
 
 extern Hjava_lang_Object* buildStackTrace(void*);
-extern void printStackTrace(struct Hjava_lang_Throwable*, struct Hjava_lang_Object*);
+extern void printStackTrace(struct Hjava_lang_Throwable*, struct Hjava_lang_Object*, int);
 
 /*
  * Fill in stack trace information - don't know what thought.
@@ -37,5 +37,5 @@ java_lang_Throwable_fillInStackTrace(struct Hjava_lang_Throwable* o)
 void
 java_lang_Throwable_printStackTrace0(struct Hjava_lang_Throwable* o, struct Hjava_lang_Object* p)
 {
-	printStackTrace(o, p);
+	printStackTrace(o, p, 0);
 }
