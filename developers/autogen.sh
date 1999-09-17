5ef51789d9df02fb58bfec52e7cddd092e6cd5ac
archie <archie>
1999-09-17 23:38:17 +0000

    Add a call to update-class-list in here.

--- developers/autogen.sh
+++ developers/autogen.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
 # This script runs all of the various auto* programs in the correct order.
+# You should run this from the top-level directory.
 # Written by Mo DeJong.
 
+( cd libraries/javalib && ../../developers/update-class-list )
 aclocal -I .
 autoheader -l config
 automake --add-missing --verbose
