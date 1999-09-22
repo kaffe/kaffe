b4bbb44bf3bc1f66937a29ff3f4df58905271874
archie <archie>
1999-09-22 23:40:52 +0000

    Rewrite this file. Previous version resulted in this exception when compiling Klasses.jar using KJC compiler version 1.3A:
    	java.lang.Error: Inconsistent Hashtable
    		at java.lang.Throwable.<init>(Throwable.java:38)
    		at java.lang.Error.<init>(Error.java:21)
    		at java.util.Hashtable.put(Hashtable.java:213)
    
    Bug was not tracked down or resolved, but this implementation
    seems to not have it.

--- libraries/javalib/java/util/Hashtable.java
+++ libraries/javalib/java/util/Hashtable.java
@@ -1,522 +1,159 @@
+
 /*
  * Java core library component.
  *
- * Copyright (c) 1997, 1998
- *      Transvirtual Technologies, Inc.  All rights reserved.
+ * Copyright (c) 1999
+ *	Archie L. Cobbs.  All rights reserved.
+ * Copyright (c) 1999
+ *	Transvirtual Technologies, Inc.  All rights reserved.
  *
  * See the file "license.terms" for information on usage and redistribution
  * of this file.
+ *
+ * Author: Archie L. Cobbs <archie@whistle.com>
  */
 
 package java.util;
 
-import java.lang.String;
 import java.io.Serializable;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.lang.ClassNotFoundException;
 
-/* Hashtable (NOT tree) with simple clustering */
+public class Hashtable extends Dictionary
+		implements Map, Cloneable, Serializable {
+	private static final long serialVersionUID = 1421746759512286392L;
+	private static final int DEFAULT_CAPACITY = 11;
+	private static final float DEFAULT_LOADFACTOR = 0.75f;
+	private final HashMap map;
 
-public class Hashtable extends Dictionary implements Map, Cloneable, Serializable {
-  private Object keys[];
-  private Object elements[];
-  private float loadFactor;
-  private int numberOfKeys;
-  private int rehashLimit;
-
-  private static final int DEFAULTCAPACITY = 101;
-  private static final float DEFAULTLOADFACTOR = (float)0.75;
-  private static final Object removed = new Object();
-  private static final Object free = null;
-
-  /* This is what Sun's JDK1.1 "serialver java.util.Hashtable" spits out */
-  private static final long serialVersionUID = 1421746759512286392L;
-
-  public Hashtable() {
-    this(DEFAULTCAPACITY, DEFAULTLOADFACTOR);
-  }
-  
-  public Hashtable(int initialCapacity) {
-    this(initialCapacity, DEFAULTLOADFACTOR);
-  }
-  
-  public Hashtable(int initialCapacity, float loadFactor)
-  {
-    if (initialCapacity <= 0) {
-      throw new Error("Initial capacity is <= 0");
-    }
-    if (loadFactor <= 0.0) {
-      throw new Error("Load Factor is <= 0");
-    }
-    this.loadFactor = loadFactor;
-    this.keys = new Object[initialCapacity];
-    this.elements = new Object[initialCapacity];
-    this.numberOfKeys = 0;
-    this.rehashLimit = (int)(loadFactor * (float)initialCapacity);
-  }
-  
-  public int size() {
-    return (numberOfKeys);
-  }
-  
-  public boolean isEmpty() {
-    return (numberOfKeys == 0);
-  }
-  
-  public synchronized Enumeration keys() {
-    Vector vector = new Vector(numberOfKeys);
-
-    for (int pos = keys.length-1; pos >= 0; pos--) {
-      if (keys[pos] != free && keys[pos] != removed) {
-	vector.addElement(keys[pos]);
-      }
-    }
-
-    return (new HashtableEnumeration(vector));
-  }
-
-
-  public synchronized Enumeration elements() {
-    Vector vector = new Vector(numberOfKeys);
-
-    for (int pos = elements.length-1; pos >= 0; pos--) {
-      if (keys[pos] != free && keys[pos] != removed) {
-	vector.addElement(elements[pos]);
-      }
-    }
-
-    return (new HashtableEnumeration(vector));
-  }
-  
-  public synchronized boolean contains(Object value) {
-    for (int pos = elements.length-1; pos >= 0; pos--) {
-      if (value.equals(elements[pos])) {
-	  return (true);
-      }
-    }
-    return false;
-  }
-  
-  public synchronized boolean containsKey(Object key) {
-    return (get(key) != null);
-  }
-  
-  private int calculateBucket(Object key) {
-    return ((key.hashCode() & Integer.MAX_VALUE) % keys.length);
-  }
-
-  public synchronized Object get(Object key)
-  {
-    int posn = calculateBucket(key);
-    int limit = keys.length;
-    for (int i = posn; i < limit; i++) {
-      Object mkey = keys[i];
-      if (key.equals(mkey)) {
-	return (elements[i]);
-      }
-      if (mkey == free) {
-	return (null);
-      }
-    }
-    for (int i = 0; i < posn; i++) {
-      Object mkey = keys[i];
-      if (key.equals(mkey)) {
-	return (elements[i]);
-      }
-      if (mkey == free) {
-	return (null);
-      }
-    }
-    return (null);
-  }
-  
-  protected synchronized void rehash()
-  {
-    int newCapacity = keys.length * 2;
-    Object oldKeys[] = keys;
-    Object oldElements[] = elements;
-
-    keys = new Object[newCapacity];
-    elements = new Object[newCapacity];
-    rehashLimit = (int)(loadFactor * (float)newCapacity);
-    numberOfKeys = 0;
-
-    /* Go through adding all the data to the new data */
-    for (int pos = oldKeys.length-1; pos >= 0; pos--) {
-      if (oldKeys[pos] != free && oldKeys[pos] != removed) {
-	put(oldKeys[pos], oldElements[pos]);
-      }
-    }
-  }
-  
-  public synchronized Object put(Object key, Object value) {
-    if (numberOfKeys >= rehashLimit) {
-      rehash();
-    }
-
-    int posn = calculateBucket(key);
-    int limit = keys.length;
-    int space = -1;
-    for (int i = posn; i < limit; i++) {
-      Object mkey = keys[i];
-      if (mkey == removed) {
-	if (space == -1) {
-	  space = i;
+	public Hashtable() {
+		this(DEFAULT_CAPACITY, DEFAULT_LOADFACTOR);
 	}
-      }
-      else if (mkey == free) {
-	if (space == -1) {
-	  space = i;
+
+	public Hashtable(int initialCapacity) {
+		this(initialCapacity, DEFAULT_LOADFACTOR);
 	}
-	keys[space] = key;
-	elements[space] = value;
-	numberOfKeys++;
-	return (null);
-      }
-      else if (key.equals(mkey)) {
-	Object oldElement = elements[i];
-	elements[i] = value;
-	return (oldElement);
-      }
-    }
-    for (int i = 0; i < posn; i++) {
-      Object mkey = keys[i];
-      if (key.equals(mkey)) {
-	Object oldElement = elements[i];
-	elements[i] = value;
-	return (oldElement);
-      }
-      if (mkey == removed) {
-	if (space == -1) {
-	  space = i;
+
+	public Hashtable(int initialCapacity, float loadFactor) {
+		map = new HashMap(initialCapacity, loadFactor);
 	}
-      }
-      else if (mkey == free) {
-	if (space == -1) {
-	  space = i;
+
+	public Hashtable(Map t) {
+		map = new HashMap(t);
 	}
-	keys[space] = key;
-	elements[space] = value;
-	numberOfKeys++;
-	return (null);
-      }
-    }
-    if (space != -1) {
-      keys[space] = key;
-      elements[space] = value;
-      numberOfKeys++;
-      return (null);
-    }
-    // We shouldn't get here.
-    throw new Error("Inconsistent Hashtable");
-  }
-  
-  public synchronized Object remove(Object key) {
 
-    int posn = calculateBucket(key);
-    int limit = keys.length;
-    for (int i = posn; i < limit; i++) {
-      Object mkey = keys[i];
-      if (key.equals(mkey)) {
-	Object oldElement = elements[i];
-	elements[i] = removed;
-	keys[i] = removed;
-	numberOfKeys--;
-	return (oldElement);
-      }
-      if (mkey == free) {
-	return (null);
-      }
-    }
-    for (int i = 0; i < posn; i++) {
-      Object mkey = keys[i];
-      if (key.equals(mkey)) {
-	Object oldElement = elements[i];
-	elements[i] = removed;
-	keys[i] = removed;
-	numberOfKeys--;
-	return (oldElement);
-      }
-      if (mkey == free) {
-	return (null);
-      }
-    }
-    return (null);
-  }
-  
-  public synchronized void clear() {
-    for (int pos = keys.length - 1; pos >= 0; pos--) {
-      keys[pos] = free;
-      elements[pos] = free;
-    }
-    numberOfKeys = 0;
-  }
-  
-  /**
-   * Creates a shallow copy of this hashtable.  
-   * The keys and values themselves are not cloned. 
-   * This is a relatively expensive operation. 
-   *
-   * @return a clone of the hashtable. 
-   */
-  public synchronized Object clone() {
-    Hashtable result;
-    try {
-      /* Note that we must use super.clone() here instead of a
-       * constructor or else subclasses such as java.util.Properties
-       * will not be cloned properly.
-       */
-      result = (Hashtable)super.clone();
-    }
-    catch (CloneNotSupportedException _) {
-      return (null);
-    }
-    result.numberOfKeys = 0;
-    result.loadFactor = loadFactor;
-    result.keys = new Object[keys.length];
-    result.elements = new Object[elements.length];
+	private Hashtable(HashMap map) {
+		this.map = map;
+	}
 
-    /* copy our entries in new hashtable */ 
-    for (int pos = keys.length-1; pos >= 0; pos--) {
-      if (keys[pos] != free && keys[pos] != removed) {
-	result.put(keys[pos], elements[pos]);
-      }
-    }
-    return ((Object)result);
-  }
-  
-  /**
-   * read this hashtable from a stream
-   */
-  private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
-    // read all default fields
-    stream.defaultReadObject();
+	public synchronized int size() {
+		return map.size();
+	}
 
-    // create buckets
-    int capacity = stream.readInt();
-    int size = stream.readInt();
+	public synchronized boolean isEmpty() {
+		return map.isEmpty();
+	}
 
-    // We ignore the capacity
-    capacity = (int)(rehashLimit / loadFactor);
+	public synchronized Enumeration keys() {
+		return new Enumeration() {
+			private final Iterator keys = keySet().iterator();
+			public boolean hasMoreElements() {
+				return keys.hasNext();
+			}
+			public Object nextElement() {
+				return keys.next();
+			}
+		};
+	}
 
-    keys = new Object[capacity];
-    elements = new Object[capacity];
+	public synchronized Enumeration elements() {
+		return new Enumeration() {
+			private final Iterator vals = values().iterator();
+			public boolean hasMoreElements() {
+				return vals.hasNext();
+			}
+			public Object nextElement() {
+				return vals.next();
+			}
+		};
+	}
 
-    // clear table, but remember how many entries are in the stream
-    numberOfKeys = 0;
+	public synchronized boolean contains(Object val) {
+		if (val == null) {
+			throw new NullPointerException();
+		}
+		return map.containsValue(val);
+	}
 
-    // read entries
-    for (int i = 0; i < size; i++) {
-      Object k = stream.readObject();
-      Object o = stream.readObject();
-      put(k, o);
-    }
-  }
+	public synchronized boolean containsKey(Object key) {
+		if (key == null) {
+			throw new NullPointerException();
+		}
+		return map.containsKey(key);
+	}
 
-  /**
-   * write this hashtable into a stream
-   */
-  private void writeObject(java.io.ObjectOutputStream stream) throws IOException {
-    // write all default fields
-    stream.defaultWriteObject();
+	public synchronized boolean containsValue(Object val) {
+		if (val == null) {
+			throw new NullPointerException();
+		}
+		return map.containsValue(val);
+	}
 
-    // remember how many buckets there were
-    stream.writeInt(keys.length);
-    stream.writeInt(numberOfKeys);
+	public synchronized Object get(Object key) {
+		if (key == null) {
+			throw new NullPointerException();
+		}
+		return map.get(key);
+	}
 
-    for (int pos = 0; pos < keys.length; pos++) {
-      if (keys[pos] != free && keys[pos] != removed) {
-	stream.writeObject(keys[pos]);
-	stream.writeObject(elements[pos]);
-      }
-    }
-  }
+	protected synchronized void rehash() {
+		map.rehash();
+	}
 
-  public synchronized String toString() {
-    boolean firstTime = true;
-    StringBuffer result = new StringBuffer();
+	public synchronized Object put(Object key, Object val) {
+		if (key == null || val == null) {
+			throw new NullPointerException();
+		}
+		return map.put(key, val);
+	}
 
-    result.append('{');
-    int pos = 0;
-    for (; pos < keys.length; pos++) {
-      if (keys[pos] != free && keys[pos] != removed) {
-	result.append(keys[pos]);
-	result.append("=");
-	result.append(elements[pos]);
-      }
-    }
-    for (; pos < keys.length; pos++) {
-      if (keys[pos] != free && keys[pos] != removed) {
-	result.append(", ");
-	result.append(keys[pos]);
-	result.append("=");
-	result.append(elements[pos]);
-      }
-    }
-    result.append('}');
+	public synchronized Object remove(Object key) {
+		return map.remove(key);
+	}
 
-    return (result.toString());
-  }
+	public synchronized void putAll(Map t) {
+		map.putAll(t);
+	}
 
-  public boolean containsValue(Object o) {
-    return (contains(o));
-  }
+	public synchronized void clear() {
+		map.clear();
+	}
 
-  public void putAll(Map m) {
-    Object[] keys = m.keySet().toArray();
-    for (int i = 0; i < keys.length; i++) {
-      put(keys[i], m.get(keys[i]));
-    }
-  }
+	public synchronized Object clone() {
+		return new Hashtable((Hashtable)map.clone());
+	}
 
-  public synchronized Set entrySet() {
-    final Vector kvec = new Vector(numberOfKeys);
-    final Vector vals = new Vector(numberOfKeys);
+	public synchronized String toString() {
+		return map.toString();
+	}
 
-    // Snapshot key list and corresponding value list
-    for (int pos = keys.length-1; pos >= 0; pos--) {
-      if (keys[pos] != free && keys[pos] != removed) {
-	kvec.addElement(keys[pos]);
-	vals.addElement(get(keys[pos]));
-      }
-    }
+	public synchronized Set keySet() {
+		return map.keySet();
+	}
 
-    // Return unmodifiable Set view (XXX should be modifyable)
-    return new AbstractSet() {
-      public int size() {
-	return kvec.size();
-      }
-      public Iterator iterator() {
-	return new Iterator() {
-	  private int index = 0;
-	  public boolean hasNext() {
-	    return index < kvec.size();
-	  }
-	  public Object next() {
-	    final Object key = kvec.elementAt(index);
-	    final Object val = vals.elementAt(index);
-	    index++;
-	    return new AbstractMapEntry(key, val) {
-	      public void changeValue(Object newValue) {
-		throw new UnsupportedOperationException();
-	      }
-	    };
-	  }
-	  public void remove() {
-	    throw new UnsupportedOperationException();
-	  }
-	};
-      }
-    };
-  }
+	public synchronized Set entrySet() {
+		return map.entrySet();
+	}
 
-  public Set keySet() {
-    final Vector kvec = new Vector(numberOfKeys);
+	public synchronized Collection values() {
+		return map.values();
+	}
 
-    // Snapshot key list
-    for (int pos = keys.length-1; pos >= 0; pos--) {
-      if (keys[pos] != free && keys[pos] != removed) {
-	kvec.addElement(keys[pos]);
-      }
-    }
+	public synchronized boolean equals(Object o) {
+		return (o instanceof Hashtable)
+		    && map.equals(((Hashtable)o).map);
+	}
 
-    // Return unmodifiable Set view (XXX should be modifyable)
-    return new AbstractSet() {
-      private int index;
-      public int size() {
-	return kvec.size();
-      }
-      public Iterator iterator() {
-	return new Iterator() {
-	  private int index = 0;
-	  public boolean hasNext() {
-	    return index < kvec.size();
-	  }
-	  public Object next() {
-	    return kvec.elementAt(index++);
-	  }
-	  public void remove() {
-	    throw new UnsupportedOperationException();
-	  }
-	};
-      }
-    };
-  }
-
-  public Collection values() {
-    final Vector vals = new Vector(numberOfKeys);
-
-    // Snapshot key list
-    for (int pos = keys.length-1; pos >= 0; pos--) {
-      if (keys[pos] != free && keys[pos] != removed) {
-	vals.addElement(get(keys[pos]));
-      }
-    }
-
-    // Return unmodifiable Collection view (XXX should be modifyable)
-    return new AbstractCollection() {
-      public int size() {
-	return vals.size();
-      }
-      public Iterator iterator() {
-	return new Iterator() {
-	  private int index = 0;
-	  public boolean hasNext() {
-	    return index < vals.size();
-	  }
-	  public Object next() {
-	    return vals.elementAt(index++);
-	  }
-	  public void remove() {
-	    throw new UnsupportedOperationException();
-	  }
-	};
-      }
-    };
-  }
-
-  /*
-   * JDK compatible serialization
-   */
-  class DefaultSerialization {
-  
-  private float loadFactor;
-  private int threshold;
-  
-  private void readDefaultObject() {
-          Hashtable.this.loadFactor = loadFactor;
-          Hashtable.this.rehashLimit = threshold;
-  }
-  
-  private void writeDefaultObject() {
-          loadFactor = Hashtable.this.loadFactor;
-          threshold = Hashtable.this.rehashLimit;
-  }
-
-  }
-
+	public synchronized int hashCode() {
+		return map.hashCode();
+	}
 }
 
-class HashtableEnumeration
-  implements Enumeration
-{
-  private Vector vector;
-  private int posn = 0;
-
-  public HashtableEnumeration(Vector vector) {
-    this.vector = vector;
-  }
-
-  public boolean hasMoreElements() {
-    return (posn < vector.size());
-  }
-
-  public Object nextElement() {
-    if (posn >= vector.size()) {
-      throw new NoSuchElementException();
-    }
-    return (vector.elementAt(posn++));
-  }
-}
