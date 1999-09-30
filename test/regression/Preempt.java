dfe436374aa6b49bdc13d0c65a89cfd706e180b8
gback <gback>
1999-09-30 17:39:21 +0000

    added test to ensure that preemption doesn't brake.

---
+++ test/regression/Preempt.java
@@ -0,0 +1,45 @@
+public class Preempt implements Runnable
+{
+    public final static int TIMEOUT = 2000;
+
+    public static void main(String args[])
+    {
+	new Thread() {
+	    public void run() {
+		try {
+		    Thread.sleep(TIMEOUT);
+		} catch (InterruptedException _) {
+		    ;
+		}
+		System.exit(0);
+	    }
+	}.start();
+
+	for (int i = 0; i < 5; i++)
+	    new Thread(new Preempt(), "thread " + i).start();
+
+	System.out.println("started 5 threads");
+    }
+
+    public void run()
+    {
+	// this shouldn't be necessary---System.out.println should be
+	// synchronized, but it apparently is (9/30/99)
+	synchronized(Preempt.class) {
+	    System.out.println(Thread.currentThread().getName());
+	}
+	while (true) {
+		continue;
+	}
+    }
+}
+
+// Sort output
+/* Expected Output:
+started 5 threads
+thread 0
+thread 1
+thread 2
+thread 3
+thread 4
+*/
