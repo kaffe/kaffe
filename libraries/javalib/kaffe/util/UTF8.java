8f3c4f3092d7c04a04bc12c97f839c8b57c7d627
archie <archie>
1999-10-06 22:46:07 +0000

    Update copyright.

--- libraries/javalib/kaffe/util/UTF8.java
+++ libraries/javalib/kaffe/util/UTF8.java
@@ -1,11 +1,16 @@
+
 /*
  * Java core library component.
  *
- * Copyright (c) 1997, 1998
- *      Transvirtual Technologies, Inc.  All rights reserved.
+ * Copyright (c) 1999
+ *	Archie L. Cobbs.  All rights reserved.
+ * Copyright (c) 1999
+ *	Transvirtual Technologies, Inc.  All rights reserved.
  *
  * See the file "license.terms" for information on usage and redistribution
  * of this file.
+ *
+ * Author: Archie L. Cobbs <archie@whistle.com>
  */
 
 package kaffe.util;
