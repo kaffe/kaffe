092e459488e6a6d2580ebef5f51ee833526b9852
tim <tim>
1999-10-10 00:32:53 +0000

    * Updated copyrights to be license-lesser.terms (LGPL).

--- libraries/extensions/rmi/javalib/java/rmi/activation/ActivationGroup.java
+++ libraries/extensions/rmi/javalib/java/rmi/activation/ActivationGroup.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 1996, 1997, 1998, 1999
+ *      Transvirtual Technologies, Inc.  All rights reserved.
+ *
+ * See the file "license-lesser.terms" for information on usage and 
+ * redistribution of this file.
+ */
+
 package java.rmi.activation;
 
 import java.rmi.server.UnicastRemoteObject;
--- libraries/extensions/rmi/javalib/java/rmi/server/RMISocketFactory.java
+++ libraries/extensions/rmi/javalib/java/rmi/server/RMISocketFactory.java
@@ -2,8 +2,8 @@
  * Copyright (c) 1996, 1997, 1998, 1999
  *      Transvirtual Technologies, Inc.  All rights reserved.
  *
- * See the file "license.terms" for information on usage and redistribution
- * of this file.
+ * See the file "license-lesser.terms" for information on usage and 
+ * redistribution of this file.
  */
 
 package java.rmi.server;
