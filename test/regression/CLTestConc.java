381d818b504429402a74e5014016b14f8cd66aa1
gback <gback>
1999-10-14 05:00:55 +0000

    added comment

--- test/regression/CLTestConc.java
+++ test/regression/CLTestConc.java
@@ -17,6 +17,39 @@ public class CLTestConc extends ClassLoader
     static boolean verbose = false;
     Thread	first, second;
 
+    /*
+     * We first create one loader and load "CLTestConc_run".
+     * Then we start three threads that attempt to create a new instance 
+     * of that class.  CLTestConc_run references CLTestConc_bis.
+     *
+     * We must ensure several things.
+     * First, we must ensure that the VM does not call out to a custom loader
+     * while another call is in progress.
+     * 
+     * Second, we must ensure that the VM attempts to
+     * load a class after its first attempt failed because the thread
+     * executing the loadClass method died.
+     *
+     * Third, we must ensure that a class is considered loaded (and all
+     * threads waiting for it) are resumed as soon as the class has been
+     * defined via defineClass.
+     *
+     * We use three threads, which can execute in arbitrary order.
+     * Thread 1 is the first one trying to load the class.
+     * We make it wait for 2 seconds and the die *before* calling defineClass.
+     * Thread 2 is the second thread trying to load the class.
+     * It loads the class's bytes and defines it via defineClass, but it
+     * dies before it has a chance to return from loadClass.
+     * Thread 3 just sits and waits until the class is loaded.  Thread 3
+     * is unblocked as soon as thread 2 defines the class and returns
+     * from loadClass (except, of course, it doesn't return---but loadClass
+     * catches ThreadDeath)
+     *
+     * Thread 3, being the only one alive, will then move on to resolve
+     * references of CLTestConc_bis to other classes.  Once that happens,
+     * we set its name to "Success", and because thread 3 is the only
+     * surviving thread, this is all that's printed.
+     */
     protected synchronized Class loadClass(String name, boolean resolve)
 	throws ClassNotFoundException
     {
@@ -31,7 +64,7 @@ public class CLTestConc extends ClassLoader
 		current.setName("Failure 1.");
 		try {
 		    /* A wrong implementation might now send the other threads
-		     * in here.  In this case, all we'll be stopped.
+		     * in here.  In this case, all will be stopped.
 		     */
 		    wait(2000);
 		} catch (InterruptedException _) { }
@@ -51,6 +84,9 @@ public class CLTestConc extends ClassLoader
 		if (verbose)
 		    System.out.println("second thread asked to load " + name);
 		second = current;
+		/* We will stop the second thread below.  Like the first thread,
+		 * it is another unlucky candidate trying to load this class
+		 */
 		second.setName("Failure 2.");
 	    } else {
 		if (verbose)
@@ -70,6 +106,9 @@ public class CLTestConc extends ClassLoader
 	    }
 	    c = defineClass(name, b, 0, b.length);
 
+	    /* The second thread dies after defining the class 
+	     * This should unblock the third thread.
+	     */
 	    if (current == second) {
 		if (verbose)
 		    System.out.println("stopping second thread");
@@ -77,6 +116,7 @@ public class CLTestConc extends ClassLoader
 		throw new ThreadDeath();
 	    }
 	} else {
+	    /* The third thread, loading stuff to which CLTestConc_* refers */
 	    if (current != second && second != null) {
 		current.setName("Success.");
 	    }
