9677011144bc50276cd8541f01aeb0cea97f9731
gback <gback>
1999-12-02 04:31:55 +0000

    added division test; kaffe currently fails this test

---
+++ test/regression/divtest.java
@@ -0,0 +1,17 @@
+/* test that we don't get tripped up by x86 problem */
+
+public class divtest {
+
+    public static int divfunc(int x, int y) {
+      return x/y;
+    }
+
+    public static void main(String args[]) {
+
+      int i = 0x80000000;
+      int j = -1;
+
+      int k = divfunc(i,j);	// x86 causes trap for no good reason
+      System.out.println("Result is "+Integer.toHexString(k));
+    }
+}
