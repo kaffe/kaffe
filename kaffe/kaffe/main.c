8450c2d9f3b82cf1baf03ed8030b24060d1dca43
archie <archie>
2000-03-09 21:52:28 +0000

    Update copyright.
    Submitted by:	Patrick Tullmann <tullmann@cs.utah.edu>

--- kaffe/kaffe/main.c
+++ kaffe/kaffe/main.c
@@ -2,7 +2,7 @@
  * main.c
  * Kick off program.
  *
- * Copyright (c) 1996-1999
+ * Copyright (c) 1996-2000
  *	Transvirtual Technologies, Inc.  All rights reserved.
  *
  * See the file "license.terms" for information on usage and redistribution 
@@ -232,7 +232,7 @@ options(char** argv)
 		}
 		else if (strcmp(argv[i], "-version") == 0) {
 			fprintf(stderr, "Kaffe Virtual Machine\n");
-			fprintf(stderr, "Copyright (c) 1996-1999\nTransvirtual Technologies, Inc.  All rights reserved\n");
+			fprintf(stderr, "Copyright (c) 1996-2000\nTransvirtual Technologies, Inc.  All rights reserved\n");
 			fprintf(stderr, "Engine: %s   Version: %s   Java Version: %s\n", engine_name, engine_version, java_version);
 			exit(0);
 		}
