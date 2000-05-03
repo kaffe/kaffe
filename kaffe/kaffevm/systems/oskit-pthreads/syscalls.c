ad60086b4b8b8c608868e5316f0beeda1e65e340
archie <archie>
2000-05-03 01:18:35 +0000

    Allow sendto() to take an optionally NULL destination address.

--- kaffe/kaffevm/systems/oskit-pthreads/syscalls.c
+++ kaffe/kaffevm/systems/oskit-pthreads/syscalls.c
@@ -218,7 +218,7 @@ static int
 oskit_pthread_sendto(int a, const void* b, size_t c, int d, 
 	const struct sockaddr* e, int f, ssize_t *out)
 {
-	*out = sendto(a, b, c, d, e, f);
+	*out = e ? sendto(a, b, c, d, e, f) : send(a, b, c, d);
 	return (*out == -1) ? errno : 0;
 }
 
