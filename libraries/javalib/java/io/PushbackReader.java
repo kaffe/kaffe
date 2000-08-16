076a4fcaa941312b831e9323d35f9dde93978580
archie <archie>
2000-08-16 18:35:26 +0000

    Added final DEFAULT_PUSHBACK_BUFFER_SIZE. Replaced buf and pos with inner class PushbackBuffer. (PushbackReader) Throws exception when buffer size <= 0. (checkIfStillOpen) Method added. Throws IOException if reader is closed (close) Release pushback buffer. (mark) Method added. (read) Check if reader is still open before reading. (read(char[], int, int)) Checks if reader is still open before reading. Checks if arguments are valid. Reduced amount of synchronized code. (ready) Checks if reader is still open. Simplified test expression. (reset) Method added. (throwMarkException) Method added. Used to throw IOException by mark and reset. (unread(int)) Checks if reader is closed. (unread(char [], int, int)) Checks if reader is closed. Validates its arguments. (validArguments) Method added. Used to validate arguments by read and unread methods.
    Submitted by:	Dalibor Topic <robilad@yahoo.com>

--- libraries/javalib/java/io/PushbackReader.java
+++ libraries/javalib/java/io/PushbackReader.java
@@ -12,92 +12,46 @@ package java.io;
 
 public class PushbackReader extends FilterReader {
 
-  private char[] buf;
-  private int pos;
+  private static final int DEFAULT_PUSHBACK_BUFFER_SIZE = 1;
+
+  private PushbackBuffer pbuf;
 
   public PushbackReader(Reader i, int size)
   {
     super(i);
-    buf = new char[size];
-    pos = buf.length;
+
+    if (size <= 0) {
+      throw new IllegalArgumentException("size <= 0");
+    }
+
+    pbuf = new PushbackBuffer(size);
   }
 
   public PushbackReader(Reader i)
   {
-    this(i, 1);
+    this(i, DEFAULT_PUSHBACK_BUFFER_SIZE);
   }
 
-  public int read() throws IOException
+  private void checkIfStillOpen() throws IOException 
   {
-    synchronized(lock) {
-
-      if (pos < buf.length) {
-        return ((int)buf[pos++]);
-      }
-      return (super.read());
-
+    if (pbuf == null) {
+      throw new IOException("Stream closed");
     }
   }
 
-  public int read(char cbuf[], int off, int len) throws IOException
+  public void close() throws IOException
   {
     synchronized(lock) {
-
-      int cnt = 0;
-      while (pos < buf.length && len > 0) {
-        cbuf[off++] = buf[pos++];
-        len--;
-        cnt++;
-      }
-      if (len > 0) {
-        int r = super.read(cbuf, off, len);
-        if (r == -1) {
-	  if (cnt == 0) {
-	    cnt = -1;
-	  }
-        }
-        else {
-	  cnt += r;
-        }
-      }
-      return (cnt);
-
+      /* Release pushback buffer. */
+      pbuf = null;
     }
+
+    super.close();
   }
 
-  public void unread(int c) throws IOException
+  public void mark(int readAheadLimit) throws IOException 
   {
-    synchronized(lock) {
-
-      if (pos == 0) {
-	throw new IOException("pushback buffer is full");
-      }
-      pos--;
-      buf[pos] = (char)c;
-    }
-  }
-
-  public void unread(char cbuf[], int off, int len) throws IOException
-  {
-    synchronized(lock) {
-
-      if (pos < len) {
-	throw new IOException("pushback buffer is too full");
-      }
-      pos -= len;
-      System.arraycopy(cbuf, off, buf, pos, len);
-
-    }
-  }
-
-  public void unread(char cbuf[]) throws IOException
-  {
-    unread(cbuf, 0, cbuf.length);
-  }
-
-  public boolean ready() throws IOException
-  {
-    return (pos < buf.length ? true : super.ready());
+    throwMarkException();
   }
 
   public boolean markSupported()
@@ -105,9 +59,182 @@ public class PushbackReader extends FilterReader {
     return (false);
   }
 
-  public void close() throws IOException
+  public int read() throws IOException
   {
-    super.close();
+    synchronized(lock) {
+
+      checkIfStillOpen();
+
+      if (pbuf.isEmpty()) {
+	return (super.read());
+      }
+      return ((int) pbuf.read());
+    }
   }
 
+  public int read(char [] cbuf, int off, int len) throws IOException
+  {
+    int cnt = 0;
+    int r   = 0;
+
+    if (!validArguments(cbuf, off, len)) {
+      throw new IndexOutOfBoundsException();
+    }
+
+    synchronized(lock) {
+
+      checkIfStillOpen();
+
+      /* Read from pushback buffer first.
+       * If it is empty, it will return 0.
+       */
+      cnt += pbuf.read(cbuf, off, len);
+      len -= cnt;
+      off += cnt;
+
+      /* If we need to read more characters,
+       * get them from the underlying reader.
+       */
+      if (len > 0) {
+        r = super.read(cbuf, off, len);
+      }
+    }
+
+    /* Calculate return code. If pushback buffer was
+     * empty, and the underlying reader returned -1,
+     * we need to return -1. Otherwise, we return the
+     * total number of characters read.
+     */
+    if (r == -1) {
+      if (cnt == 0) {
+	cnt = -1;
+      }
+    }
+    else {
+      cnt += r;
+    }
+    
+    return (cnt);
+  }
+
+  public boolean ready() throws IOException 
+  {
+    synchronized(lock) {
+
+      checkIfStillOpen();
+
+      return (!pbuf.isEmpty() || super.ready());
+    }
+  }
+
+  public void reset() throws IOException 
+  {
+    throwMarkException();
+  }
+
+  private static void throwMarkException() throws IOException
+  {
+    throw new IOException("mark/reset not supported");
+  }
+
+  public void unread(int c) throws IOException
+  {
+    synchronized(lock) {
+
+      checkIfStillOpen();
+
+      pbuf.unread((char) c);
+    }
+  }
+
+  public void unread(char [] cbuf, int off, int len) throws IOException
+  {
+    if (!validArguments(cbuf, off, len)) {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+
+    synchronized(lock) {
+
+      checkIfStillOpen();
+
+      pbuf.unread(cbuf, off, len);
+    }
+  }
+
+  public void unread(char [] cbuf) throws IOException
+  {
+    unread(cbuf, 0, cbuf.length);
+  }
+
+  private static boolean validArguments(char [] cbuf, int off, int len) 
+  {
+    if (0 <= off && off < cbuf.length && 0 <= len && len <= cbuf.length - off) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private class PushbackBuffer {
+
+    private char[] buf;
+    private int pos;
+
+    private PushbackBuffer(int size) {
+      buf = new char[size];
+      pos = buf.length;
+    }
+
+    private int capacity() {
+      return pos;
+    }
+
+    private boolean isEmpty() {
+      return (capacity() == buf.length);
+    }
+
+    private boolean isFull() {
+      return (capacity() == 0);
+    }
+
+    /* length() returns the number of pushed-back
+     * characters left to read.
+     */
+    private int length() {
+      return (buf.length - capacity());
+    }
+
+    private char read() {
+      return (buf[pos++]);
+    }
+
+    private int read(char [] cbuf, int off, int len) {
+      len = Math.min(len, length());
+      System.arraycopy(buf, pos, cbuf, off, len);
+      pos += len;
+      return len;
+    }
+
+    private void throwOverflowException() throws IOException {
+      throw new IOException("Pushback buffer overflow");
+    }
+
+    private void unread(char c) throws IOException {
+      if (this.isFull()) {
+	throwOverflowException();
+      }
+
+      buf[--pos] = c;
+    }
+
+    private void unread(char [] cbuf, int off, int len) throws IOException {
+      if (len > capacity()) {
+	throwOverflowException();
+      }
+
+      pos -= len;
+      System.arraycopy(cbuf, off, buf, pos, len);
+    } 
+  }
 }
+
