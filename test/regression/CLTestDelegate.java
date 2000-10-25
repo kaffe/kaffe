7a1c5ef360788dc13ab2287b138de6d12268ec0c
jbaker <jbaker>
2000-10-25 16:28:40 +0000

    A new regression test.  More than a year ago, we regressed to the point where finalizeClassLoader is no longer run.  This test will trigger the destroyClass sanity check if there are dangling ClassPool entries.

---
+++ test/regression/CLTestDelegate.java
@@ -0,0 +1,148 @@
+import java.io.*;
+
+/**
+ * Can we gc classes which where once accessible from multiple
+ * loaders, or does something die in the process.
+ *
+ * @author <a href=mailto://jbaker@cs.utah.edu> Jason Baker </a>
+ */
+public class CLTestDelegate extends ClassLoader {
+	static boolean firstDead = false;
+	static boolean secondDead = false;
+	static boolean verbose = false;
+
+	static String CNAME = "CLTestDelegate$C";
+	static Class def = null;
+	static byte[] bytes = readin(CNAME + ".class");
+
+	static String DNAME = "CLTestDelegate$D";
+	static class C {
+	}
+
+	static class D {
+		static C dummy = new C();
+		static {
+			if (verbose)
+				System.err.println("D is loaded");
+		}
+	}
+
+	boolean amFirst = true;
+
+	protected Class loadClass(String name, boolean resolve)
+		throws ClassNotFoundException
+	{
+		Class c;
+		if (name.equals(CNAME)) {
+			if (def == null) 
+				def = defineClass(bytes, 0, bytes.length);
+			else
+				amFirst = false;
+			c = def;
+		} else if (name == DNAME) {
+			byte[] dBytes = readin(DNAME + ".class");
+			c = defineClass(dBytes, 0, dBytes.length);
+		} else {
+			 c = findSystemClass(name);
+		}
+		resolveClass(c);
+		return c;
+	}
+
+	static byte [] readin(String name) 
+	{
+		try {
+			File cf = new File(name);
+			FileInputStream cfi = new FileInputStream(cf);
+
+			int len = (int)cf.length();
+			byte [] cb = new byte[len];
+			if (cfi.read(cb) != len) {
+				System.err.println("short read for " + name);
+				throw new RuntimeException();
+			}
+			return cb;
+		} catch (IOException e) {
+			System.err.println("couldn't read " + name
+					   + ": " + e.getMessage());
+			throw new RuntimeException();
+		}
+	}
+
+	protected void finalize() {
+		if (verbose)
+			System.err.println((amFirst ? "first " : "second ")
+					   + "dying");
+		if (amFirst)
+			firstDead = true;
+		else
+			secondDead = true;
+	}
+
+	/*
+	public static String trashStack(int count)
+	{
+		if (count > 0)
+			return "trash: " + trashStack(count - 1);
+		else
+			return "gone from stack";
+	}
+
+	static String dummy;
+	*/
+	public static void doit()
+	{
+		try {
+			// The first loader defines a class
+			new CLTestDelegate().loadClass(DNAME).newInstance();
+
+			// The second loader delagates to the first
+			new CLTestDelegate().loadClass(DNAME).newInstance();
+
+			def = null;
+		}
+		catch (Exception e) {
+			throw new RuntimeException("No such luck : "
+						   + e.getMessage());
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		doit();
+
+		// As of 10-25-00, it takes a minimum of 3 gcs to
+		// manifest the problem, and this argument parsing
+		// code seems to be needed to clean up the stack. 
+		int gcCount = 6;
+		int idx = 0;
+		while (args.length > idx) {
+			if (args[idx].equals("-c")
+			    && args.length > idx + 1)
+				gcCount = Integer.parseInt(args[++idx]);
+			else if (args[idx] == "-v") 
+				verbose = true;
+			else {
+				System.err.println
+					("usage: CLTestDelegate "
+					 + " {-c GC-COUNT}?"
+					 + " -v?");
+			}
+			idx++;
+		}
+
+		// give time for finalizers to run, and for classes
+		// our loaders kept alive to be collected
+		for (int i = 0; i < gcCount; i++) 
+			System.gc();
+		if (!(firstDead && secondDead))
+			System.err.println("ClassLoaders not GCed");
+		else
+			// If we didn't crash in destroyClass, life is
+			// good. 
+			System.out.println("success");
+	}
+}
+/* Expected Output:
+success
+*/
