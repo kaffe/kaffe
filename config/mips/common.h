3c2eb81e1971992aaf4dfe38b654e720499d6f63
egp <egp>
2001-02-11 23:11:01 +0000

    (COMPARE_AND_EXCHANGE): Create safe version.  Note that is use MIPS II instructions ll/sc and does not work on R3000.

--- config/mips/common.h
+++ config/mips/common.h
@@ -38,4 +38,55 @@
 
 #endif /* NEED_sysdepCallMethod */
 
+
+#if defined(HAVE_MIPSII_INSTRUCTIONS)
+/*
+ * Do an atomic compare and exchange.  The address 'A' is checked against
+ * value 'O' and if they match it's exchanged with value 'N'.
+ * We return '1' if the exchange is sucessful, otherwise 0.
+ */
+#define	COMPARE_AND_EXCHANGE(A,O,N)		\
+({						\
+	unsigned int tmp, ret;			\
+ 						\
+	asm volatile(				\
+	"	.set	noreorder\n"		\
+	"	.set	mips2\n"		\
+	"1:	ll	%0, %3\n"		\
+	"	xor	%1, %0, %4\n"		\
+	"	sltiu	%1, %1, 1\n"		\
+	"	movn	%0, %5, %1\n"		\
+	"	sc	%0, %2\n"		\
+	"	beqz	%0, 1b\n"		\
+	"	nop\n"				\
+	"	.set	mips0\n"		\
+	"	.set	reorder\n"		\
+	: "=&r" (tmp), "=&r" (ret), "=m" (*(A))	\
+	: "m" (*(A)), "r" (O), "r" (N)		\
+	: "memory");				\
+	ret;					\
+})
+
+#else
+
+/*
+ * Do an atomic compare and exchange.  The address 'A' is checked against
+ * value 'O' and if they match it's exchanged with value 'N'.
+ * We return '1' if the exchange is sucessful, otherwise 0.
+ */
+#define COMPARE_AND_EXCHANGE(A,O,N)		\
+({						\
+    int ret = 0;				\
+    jthread_suspendall();			\
+						\
+    if (*(A) == (O)) {				\
+	*(A) = (N);				\
+	ret = 1;				\
+    }						\
+    jthread_unsuspendall();			\
+    ret;					\
+})
+
+#endif
+    
 #endif
