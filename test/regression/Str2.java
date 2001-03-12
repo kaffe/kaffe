d0ca696c14cd3e5d2c93265e4692c1e086235793
egp <egp>
2001-03-12 13:00:20 +0000

    Add substring lookup test with lastIndexOf() for Oliver Lombart fix.

--- test/regression/Str2.java
+++ test/regression/Str2.java
@@ -11,7 +11,21 @@ class Str2 {
 		System.out.println("abc".lastIndexOf("", 99999999));
 		System.out.println("".lastIndexOf("a"));
 		System.out.println("".lastIndexOf(""));
+
+		substring();
 	}
+
+	/** Substring match.
+	 *  @author Peter Graves <peter@armedbear.org>
+	 */
+	public static void substring() {
+		String s1 = "this is a test yes it is";
+		String s2 = "yet another test";
+		String s3 = s2.substring(12); // "test"
+		for (int i = 0; i < s1.length(); i++)
+			System.out.println("i = " + i + " index = " + s1.lastIndexOf(s3, i));
+	}
+	
 }
 
 /* Expected Output:
@@ -24,4 +38,28 @@ class Str2 {
 3
 -1
 0
+i = 0 index = -1
+i = 1 index = -1
+i = 2 index = -1
+i = 3 index = -1
+i = 4 index = -1
+i = 5 index = -1
+i = 6 index = -1
+i = 7 index = -1
+i = 8 index = -1
+i = 9 index = -1
+i = 10 index = 10
+i = 11 index = 10
+i = 12 index = 10
+i = 13 index = 10
+i = 14 index = 10
+i = 15 index = 10
+i = 16 index = 10
+i = 17 index = 10
+i = 18 index = 10
+i = 19 index = 10
+i = 20 index = 10
+i = 21 index = 10
+i = 22 index = 10
+i = 23 index = 10
 */
