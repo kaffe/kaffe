156c0cd6cf68ce2ec1f0880702fb0cebf3a6fcf9
egp <egp>
2001-05-07 10:04:30 +0000

    more division and modulo tests.

--- test/regression/divtest.java
+++ test/regression/divtest.java
@@ -6,15 +6,59 @@ public class divtest {
       return x/y;
     }
 
+    public static int modfunc(int x, int y) {
+      return x%y;
+    }
+
+    public static void test(String s, int x, int y) {
+	int r;
+
+	try {
+	    r = divfunc(x, y);
+	    System.out.println (s + " / " + Integer.toHexString(r));
+	}
+	catch (Throwable t) {
+	    System.out.println (s + " / " + t.toString());
+	}
+
+	try {
+	    r = modfunc(x, y);
+	    System.out.println (s + " % " + Integer.toHexString(r));
+	}
+	catch (Throwable t) {
+	    System.out.println (s + " % " + t.toString());
+	}
+    }
+
     public static void main(String args[]) {
+      test ("40000000 1", 0x40000000, 1);
+      test ("80000000 1", 0x80000000, 1);
 
-      int i = 0x80000000;
-      int j = -1;
+      test ("40000000 -1", 0x40000000, -1);
+      test ("80000000 -1", 0x80000000, -1);	// x86 causes trap for no good reason
 
-      int k = divfunc(i,j);	// x86 causes trap for no good reason
-      System.out.println("Result is "+Integer.toHexString(k));
+      test ("40000000 2", 0x40000000, 2);
+      test ("80000000 2", 0x80000000, 2);
+
+      test ("40000000 -2", 0x40000000, -2);
+      test ("80000000 -2", 0x80000000, -2);
     }
 }
 /* Expected Output:
-Result is 80000000
+40000000 1 / 40000000
+40000000 1 % 0
+80000000 1 / 80000000
+80000000 1 % 0
+40000000 -1 / c0000000
+40000000 -1 % 0
+80000000 -1 / 80000000
+80000000 -1 % 0
+40000000 2 / 20000000
+40000000 2 % 0
+80000000 2 / c0000000
+80000000 2 % 0
+40000000 -2 / e0000000
+40000000 -2 % 0
+80000000 -2 / 40000000
+80000000 -2 % 0
 */
