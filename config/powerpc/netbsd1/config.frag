4102bfac5fb92fd1ec5ad270851a24029d3c0127
egp <egp>
2001-06-17 16:40:50 +0000

    NetBSD PowerPC port.  From Yong Chen <ychen@allegronetworks.com>.

---
+++ config/powerpc/netbsd1/config.frag
@@ -0,0 +1,6 @@
+#
+# PowerPC/NetBSD configuration
+#
+Khost_cpu=powerpc
+Khost_os=netbsd1
+CFLAGS="-g -fsigned-char"
---
+++ config/powerpc/netbsd1/md.h
@@ -0,0 +1,21 @@
+/*
+ * powerpc/netbsd1/md.h
+ * NetBSD PowerPC configuration information.
+ *
+ * Copyright (c) 1996, 1997
+ *	Transvirtual Technologies, Inc.  All rights reserved.
+ *
+ * See the file "license.terms" for information on usage and redistribution
+ * of this file.
+ */
+
+#ifndef __powerpc_netbsd1_md_h
+#define __powerpc_netbsd1_md_h
+
+#include "powerpc/common.h"
+#include "powerpc/threads.h"
+
+#undef SP_OFFSET
+#define SP_OFFSET	3
+
+#endif
