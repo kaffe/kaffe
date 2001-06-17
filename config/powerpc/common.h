2177dc38033ec056f17aac31c0b58f1b0536f6a4
egp <egp>
2001-06-17 16:40:44 +0000

    be SMP safe.

--- config/powerpc/common.h
+++ config/powerpc/common.h
@@ -11,14 +11,20 @@
 
 #define NEED_STACK_ALIGN
 #define STACK_ALIGN(p)  ((((unsigned long)(p)) & 15) ^ (unsigned long)(p))
- 
-#if defined (__linux__) && defined(__powerpc__)
+
+#if defined(EGP)
+#if NEED_sysdepCallMethod
+#include "callmethod_v4.h"
+#endif
+#else
+#if defined(__linux__) || defined(__NetBSD__)
 #if NEED_sysdepCallMethod
 #include "callmethod_ppc.h"
 #define	sysdepCallMethod(CALL) sysdepCallMethod_ppc(CALL)
 #endif
-
 #endif
+#endif /* defined(EGP) */
+
 
 /*
  * Do an atomic compare and exchange.  The address 'A' is checked against
@@ -30,6 +36,7 @@
 	int tmp, ret = 0;			\
 						\
 	asm volatile(				\
+	"	eieio\n"			\
 	"1:	lwarx	%0,0,%3\n"		\
 	"	cmpw	0,%0,%4\n"		\
 	"	bne	2f\n"			\
@@ -46,7 +53,3 @@
 })
 
 #endif
-
-
-
-
