18a4b538e25efbdacc623b4953752fc437cdb3e4
egp <egp>
2001-06-18 10:40:39 +0000

    (ATOMIC_EXCHANGE): mark output register as early clobber.

--- config/sparc/common.h
+++ config/sparc/common.h
@@ -29,6 +29,29 @@
  * with value 'N'.
  */
 #define ATOMIC_EXCHANGE(A,N) \
-	asm volatile("swap [%2],%0" : "=r" (N) : "0" (N), "r" (A) : "cc", "memory" );
+	asm volatile("swap [%2],%0" : "=&r" (N) : "0" (N), "r" (A) : "cc", "memory" );
+
+#if 0
+#undef ATOMIC_EXCHANGE
+#warning "jthread COMPARE_AND_EXCHANGE"
+
+/*
+ * Do an atomic compare and exchange.  The address 'A' is checked against
+ * value 'O' and if they match it's exchanged with value 'N'.
+ * We return '1' if the exchange is sucessful, otherwise 0.
+ */
+#define COMPARE_AND_EXCHANGE(A,O,N)		\
+({						\
+    int ret = 0;				\
+    jthread_suspendall();			\
+						\
+    if (*(A) == (O)) {				\
+	*(A) = (N);				\
+	ret = 1;				\
+    }						\
+    jthread_unsuspendall();			\
+    ret;					\
+})
+#endif
 
 #endif
