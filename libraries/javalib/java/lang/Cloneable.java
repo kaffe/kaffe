bc3a710fe6d084b7510726cd4216c571c0b9c393
jim <jim>
2002-06-03 02:14:12 +0000

    Bumped java.lang.Cloneable version number

--- libraries/javalib/java/lang/Cloneable.java
+++ libraries/javalib/java/lang/Cloneable.java
@@ -18,5 +18,5 @@ public interface Cloneable {
 	 * This is our sanity check to guard against people using the
 	 * wrong jar file.
 	 */
-        int KAFFE_VERSION = 106;
+        int KAFFE_VERSION = 107;
 }
