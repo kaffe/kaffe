973005f5c21cd1296fb2c95d775fdc6b5bd3cc03
dalibor <dalibor>
2002-07-04 01:15:53 +0000

    merged with pocketlinux kaffe

--- libraries/javalib/java/awt/image/ImageFilter.java
+++ libraries/javalib/java/awt/image/ImageFilter.java
@@ -2,6 +2,14 @@ package java.awt.image;
 
 import java.util.Hashtable;
 
+/**
+ * Copyright (c) 2000
+ *      Transvirtual Technologies, Inc.  All rights reserved.
+ *
+ * See the file "license.terms" for information on usage and redistribution
+ * of this file.
+ *
+ */
 public class ImageFilter
   implements ImageConsumer, Cloneable
 {
@@ -22,7 +30,8 @@ public ImageFilter getFilterInstance( ImageConsumer ic) {
 }
 
 public void imageComplete( int status) {
-	consumer.imageComplete( status);
+	if ( consumer != null )
+		consumer.imageComplete( status);
 }
 
 public void resendTopDownLeftRight( ImageProducer ip) {
@@ -30,26 +39,32 @@ public void resendTopDownLeftRight( ImageProducer ip) {
 }
 
 public void setColorModel( ColorModel model) {
-	consumer.setColorModel( model);
+	if ( consumer != null )
+		consumer.setColorModel( model);
 }
 
 public void setDimensions( int width, int height) {
-	consumer.setDimensions( width, height);
+	if ( consumer != null )
+		consumer.setDimensions( width, height);
 }
 
 public void setHints( int hints) {
-	consumer.setHints( hints);
+	if ( consumer != null )
+		consumer.setHints( hints);
 }
 
 public void setPixels( int x, int y, int w, int h, ColorModel model, byte[] pels, int off, int scan) {
-	consumer.setPixels( x, y, w, h, model, pels, off, scan);
+	if ( consumer != null )
+		consumer.setPixels( x, y, w, h, model, pels, off, scan);
 }
 
 public void setPixels( int x, int y, int w, int h, ColorModel model, int[] pels, int off, int scan) {
-	consumer.setPixels( x, y, w, h, model, pels, off, scan);
+	if ( consumer != null )
+		consumer.setPixels( x, y, w, h, model, pels, off, scan);
 }
 
 public void setProperties( Hashtable props) {
-	consumer.setProperties( props);
+	if ( consumer != null )
+		consumer.setProperties( props);
 }
 }
