6756773252b857dc525bf0409a11daacbe97995a
dalibor <dalibor>
2003-01-11 19:38:19 +0000

    forgot to check in this test last time around

---
+++ test/regression/InputStreamTest.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class InputStreamTest {
+        static public void main (String args[]) {
+                try {
+                        StringBufferInputStream sbis =
+                                new
+StringBufferInputStream("foobar\r\nxbarfoo\rx");
+                        DataInputStream dis = new DataInputStream(sbis);
+
+                        System.out.println(dis.readLine());
+                        System.out.println(sbis.read());
+                        System.out.println(dis.readLine());
+                        System.out.println(sbis.read());
+			System.out.println(dis.read());
+
+                } catch (IOException ioe) {
+                }
+        }
+}
+
+/* Expected Output:
+foobar
+120
+barfoo
+-1
+120
+*/
+
