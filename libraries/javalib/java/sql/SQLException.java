c8511c14c847f3fe8cde87193ea51c36fb3cd1ff
dalibor <dalibor>
2003-04-10 09:30:59 +0000

    2003-04-10 Dalibor Topic <robilad@yahoo.com>
            * libraries/javalib/java/sql/Array.java,
            libraries/javalib/java/sql/Blob.java,
            libraries/javalib/java/sql/CallableStatement.java,
            libraries/javalib/java/sql/Clob.java,
            libraries/javalib/java/sql/Connection.java,
            libraries/javalib/java/sql/DataTruncation.java,
            libraries/javalib/java/sql/DatabaseMetaData.java,
            libraries/javalib/java/sql/Driver.java,
            libraries/javalib/java/sql/DriverPropertyInfo.java,
            libraries/javalib/java/sql/PreparedStatement.java,
            libraries/javalib/java/sql/ResultSet.java,
            libraries/javalib/java/sql/ResultSetMetaData.java,
            libraries/javalib/java/sql/SQLException.java,
            libraries/javalib/java/sql/SQLWarning.java,
            libraries/javalib/java/sql/Statement.java,
            libraries/javalib/java/sql/Types.java:
            replaced by the more up to date implementation from GNU Classpath.
    
            Reported by: Ito Kazumitsu <ito.kazumitsu@hitachi-cable.co.jp>

--- libraries/javalib/java/sql/SQLException.java
+++ libraries/javalib/java/sql/SQLException.java
@@ -1,61 +1,167 @@
-/*
- * Java core library component.
- *
- * Copyright (c) 1998
- *      Transvirtual Technologies, Inc.  All rights reserved.
- *
- * See the file "license.terms" for information on usage and redistribution
- * of this file.
- */
+/* SQLException.java -- General SQL exception
+   Copyright (C) 1999, 2000 Free Software Foundation, Inc.
+
+This file is part of GNU Classpath.
+
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+Linking this library statically or dynamically with other modules is
+making a combined work based on this library.  Thus, the terms and
+conditions of the GNU General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module.  An independent module is a module which is not derived from
+or based on this library.  If you modify this library, you may extend
+this exception to your version of the library, but you are not
+obligated to do so.  If you do not wish to do so, delete this
+exception statement from your version. */
+
 
 package java.sql;
 
-public class SQLException
-  extends Exception {
+/**
+ * This exception is thrown when a database error occurs.
+ *
+ * @author Aaron M. Renn (arenn@urbanophile.com)
+ */
+public class SQLException extends Exception 
+{
+  static final long serialVersionUID = 2135244094396331484L;
 
-// NB: these are part of the serial form;
-private String SQLState;
-private int vendorCode;
-private SQLException next;
+  /**
+   * This is the next exception in the chain
+   */
+  private SQLException next;
 
-public SQLException(String reason, String SQLState, int vendorCode) {
-	super(reason);
-	this.SQLState = SQLState;
-	this.vendorCode = vendorCode;
-	next = null;
-}
+  /**
+   * This is the state of the SQL statement at the time of the error.
+   */
+  private String SQLState;
 
-public SQLException(String reason, String SQLState) {
-	this(reason, SQLState, 0);
-}
+  /**
+   * The vendor error code for this error
+   */
+  private int vendorCode;
 
-public SQLException(String reason) {
-	this(reason, null, 0);
-}
+  /**
+   * This method initializes a nwe instance of <code>SQLException</code>
+   * with the specified descriptive error message, SQL state string, and
+   * vendor code.
+   *
+   * @param message A string describing the nature of the error.
+   * @param SQLState A string containing the SQL state of the error.
+   * @param vendorCode The vendor error code associated with this error.
+   */
+  public SQLException(String message, String SQLState, int vendorCode)
+  {
+    super(message);
+    this.SQLState = SQLState;
+    this.vendorCode = vendorCode;  
+  }
 
-public SQLException() {
-	this(null, null, 0);
-}
+  /**
+   * This method initializes a new instance of <code>SQLException</code>
+   * with the specified descriptive error message and SQL state string.
+   * The vendor error code of this instance will be 0.
+   *
+   * @param message A string describing the nature of the error.
+   * @param SQLState A string containing the SQL state of the error.
+   */
+  public SQLException(String message, String SQLState)
+  {
+    this(message, SQLState, 0);
+  }
 
-public String getSQLState() {
-	return (SQLState);
-}
+  /**
+   * This method initializes a new instance of <code>SQLException</code>
+   * with the specified descriptive error message.  The SQL state of this
+   * instance will be <code>null</code> and the vendor error code will be 0.
+   *
+   * @param message A string describing the nature of the error.
+   */
+  public SQLException(String message)
+  {
+    this(message, null, 0);  
+  }
 
-public int getErrorCode() {
-	return (vendorCode);
-}
+  /**
+   * This method initializes a new instance of <code>SQLException</code>
+   * that does not have a descriptive messages and SQL state, and which
+   * has a vendor error code of 0.
+   */
+  public SQLException()
+  {
+    this(null, null, 0);  
+  }
 
-public SQLException getNextException() {
-	return (next);
-}
+  /**
+   * This method returns the SQLState information associated with this
+   * error.  The value returned is a <code>String</code> which is formatted
+   * using the XOPEN SQL state conventions.
+   *
+   * @return The SQL state, which may be <code>null</code>.
+   */
+  public String getSQLState()
+  {
+    return SQLState;
+  }
 
-public synchronized void setNextException(SQLException ex) {
-	if (next == null) {
-		next = ex;
-	}
-	else {
-		next.setNextException(ex);
-	}
-}
+  /**
+   * This method returns the vendor specific error code associated with 
+   * this error.
+   *
+   * @return The vendor specific error code associated with this error.
+   */
+  public int getErrorCode()
+  {
+    return vendorCode;
+  }
 
+  /**
+   * This method returns the exception that is chained to this object.
+   *
+   * @return The exception chained to this object, which may be 
+   *         <code>null</code>.
+   */
+  public SQLException getNextException()
+  {
+    return next;
+  }
+
+  /**
+   * This method adds a new exception to the end of the chain of exceptions
+   * that are chained to this object.
+   *
+   * @param e The exception to add to the end of the chain.
+   */
+  public void setNextException(SQLException e)
+  {
+    if (e == null)
+      return;
+
+    SQLException list_entry = this;
+    while (list_entry.getNextException() != null)
+      list_entry = list_entry.getNextException();
+
+    list_entry.next = e;
+  }
 }
