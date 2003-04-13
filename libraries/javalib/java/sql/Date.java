7cd06e5958cfa918ebbba65d693f4162decd0a1f
dalibor <dalibor>
2003-04-13 09:02:16 +0000

    2003-04-13 Dalibor Topic <robilad@yahoo.com>
            * libraries/javalib/java/sql/Date.java,
            libraries/javalib/java/sql/Time.java,
            libraries/javalib/java/sql/Timestamp.java,
            libraries/javalib/java/sql/DriverManager.java:
            replaced by the more up to date implementation from GNU Classpath.
    
            Reported by: Ito Kazumitsu <ito.kazumitsu@hitachi-cable.co.jp>

--- libraries/javalib/java/sql/Date.java
+++ libraries/javalib/java/sql/Date.java
@@ -1,72 +1,184 @@
-/*
- * Java core library component.
- *
- * Copyright (c) 1998
- *      Transvirtual Technologies, Inc.  All rights reserved.
- *
- * See the file "license.terms" for information on usage and redistribution
- * of this file.
- */
+/* Date.java -- Wrapper around java.util.Date
+   Copyright (C) 1999, 2000 Free Software Foundation, Inc.
+
+This file is part of GNU Classpath.
+
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+Linking this library statically or dynamically with other modules is
+making a combined work based on this library.  Thus, the terms and
+conditions of the GNU General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module.  An independent module is a module which is not derived from
+or based on this library.  If you modify this library, you may extend
+this exception to your version of the library, but you are not
+obligated to do so.  If you do not wish to do so, delete this
+exception statement from your version. */
 
 package java.sql;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.text.ParsePosition;
 
-
-public class Date
-  extends java.util.Date {
-
-static private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-    
-public Date(int year, int month, int day) {
-	super(year, month, day);
-}
-
-public Date(long date) {
-	super(date);
-}
-
-public void setTime(long date) {
-	super.setTime(date);
-}
-
-public static Date valueOf(String s)
-	throws IllegalArgumentException
+/**
+  * This class is a wrapper around java.util.Date to allow the JDBC
+  * driver to identify the value as a SQL Date.
+  *
+  * @author Aaron M. Renn (arenn@urbanophile.com)
+  */
+public class Date extends java.util.Date 
 {
-	java.util.Date date = sdf.parse(s, new ParsePosition(0));
-	if (date == null) {
-		throw new IllegalArgumentException(s);
+  static final long serialVersionUID = 1511598038487230103L;
+
+  /**
+   * Used for parsing and formatting this date.
+   */
+  private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+  /**
+   * This method initializes a new instance of this class with the
+   * specified year, month, and day.
+   *
+   * @param year The year of this date minue 1900.
+   * @param month The month of this date (0-11).
+   * @param day The day of this date (1-31).
+   *
+   * @deprecated
+   */
+  public Date(int year, int month, int day)
+  {
+    super(year, month, day);  
+  }
+
+  /**
+   * This method initializes a new instance of this class with the
+   * specified time value representing the number of seconds since 
+   * Jan 1, 1970 at 12:00 midnight GMT.
+   *
+   * @param time The time value to intialize this date to.
+   */
+  public Date(long date)
+  {
+    super(date);
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public int getHours() throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public int getMinutes() throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public int getSeconds() throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public void setHours(int newValue) throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public void setMinutes(int newValue) throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public void setSeconds(int newValue) throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method returns a new instance of this class by parsing a
+   * date in JDBC format into a Java date.
+   *
+   * @param str The string to parse.
+   * @return The resulting <code>java.sql.Date</code> value.
+   *
+   * @deprecated
+   */
+  public static Date valueOf (String str)
+  {
+    try 
+      {
+	java.util.Date d = (java.util.Date) sdf.parseObject(str);
+
+	if (d == null) {
+	  throw new IllegalArgumentException(str);
 	}
-	return new Date(date.getTime());
-}
+	else {
+	  return(new Date(d.getTime()));
+	}
+      }
+    catch (ParseException e)
+      {
+	throw new IllegalArgumentException(str);
+      }
+  }
 
-public String toString() {
-	return sdf.format(this);
-}
-
-public int getHours() {
-	return (super.getHours());
-}
-
-public int getMinutes() {
-	return (super.getMinutes());
-}
-
-public int getSeconds() {
-	return (super.getSeconds());
-}
-
-public void setHours(int i) {
-	super.setHours(i);
-}
-
-public void setMinutes(int i) {
-	super.setMinutes(i);
-}
-
-public void setSeconds(int i) {
-	super.setSeconds(i);
-}
-
+  /**
+   * This method returns this date in JDBC format.
+   *
+   * @return This date as a string.
+   *
+   * @deprecated
+   */
+  public String toString()
+  {
+    return sdf.format(this);
+  }
 }
--- libraries/javalib/java/sql/Time.java
+++ libraries/javalib/java/sql/Time.java
@@ -1,81 +1,200 @@
-/*
- * Java core library component.
- *
- * Copyright (c) 1998
- *      Transvirtual Technologies, Inc.  All rights reserved.
- *
- * See the file "license.terms" for information on usage and redistribution
- * of this file.
- */
+/* Time.java -- Wrapper around java.util.Date
+   Copyright (C) 1999, 2000, 2002 Free Software Foundation, Inc.
+
+This file is part of GNU Classpath.
+
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+Linking this library statically or dynamically with other modules is
+making a combined work based on this library.  Thus, the terms and
+conditions of the GNU General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module.  An independent module is a module which is not derived from
+or based on this library.  If you modify this library, you may extend
+this exception to your version of the library, but you are not
+obligated to do so.  If you do not wish to do so, delete this
+exception statement from your version. */
+
 
 package java.sql;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.text.ParsePosition;
-import java.util.Date;
 
-
-public class Time
-  extends Date {
-
-static private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-
-public Time(int hour, int minute, int second) {
-	super();
-	setHours(hour);
-	setMinutes(minute);
-	setSeconds(second);
-}
-
-public Time(long time) {
-	super(time);
-}
-
-public void setTime(long time) {
-	super.setTime(time);
-}
-
-public static Time valueOf(String s)
-	throws IllegalArgumentException
+/**
+ * This class is a wrapper around java.util.Date to allow the JDBC
+ * driver to identify the value as a SQL Time.
+ *
+ * @author Aaron M. Renn (arenn@urbanophile.com)
+ */
+public class Time extends java.util.Date
 {
-	ParsePosition pos = new ParsePosition(0);
-	Date date = sdf.parse(s, pos);
-	if (date == null) {
-		throw new IllegalArgumentException(s);
+  static final long serialVersionUID = 8397324403548013681L;
+
+  /**
+   * Used for parsing and formatting this date.
+   */
+  private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public int getDate() throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public int getDay() throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public int getMonth() throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public int getYear() throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public void setDate(int newValue) throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public void setMonth(int newValue) throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method always throws an IllegalArgumentException.
+   *
+   * @throws IllegalArgumentException when it's called.
+   * @deprecated
+   */
+  public void setYear(int newValue) throws IllegalArgumentException {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * This method returns a new instance of this class by parsing a
+   * date in JDBC format into a Java date.
+   *
+   * @param str The string to parse.
+   * @return The resulting <code>java.sql.Time</code> value.
+   *
+   * @deprecated
+   */
+  public static Time valueOf (String str)
+  {
+    try
+      {
+	java.util.Date d = (java.util.Date) sdf.parseObject(str);
+
+	if (d == null) {
+	  throw new IllegalArgumentException(str);
 	}
-	return new Time(date.getTime());
+	else {
+	  return new Time(d.getTime());
+	}
+      }
+    catch (ParseException e)
+      {
+	throw new IllegalArgumentException(str);
+      }
+  }
+
+  /**
+    * This method initializes a new instance of this class with the
+    * specified year, month, and day.
+    *
+    * @param hour The hour for this Time (0-23)
+    * @param minute The minute for this time (0-59)
+    * @param second The second for this time (0-59)
+    * @deprecated
+    */
+  public Time(int hour, int minute, int second)
+  {
+    super(System.currentTimeMillis());
+
+    setHours(hour);
+    setMinutes(minute);
+    setSeconds(second);
+  }
+
+  /**
+   * This method initializes a new instance of this class with the
+   * specified time value representing the number of seconds since 
+   * Jan 1, 1970 at 12:00 midnight GMT.
+   *
+   * @param time The time value to intialize this <code>Time</code> to.
+   */
+  public Time(long date)
+  {
+    super(date);
+  }
+
+  /**
+   * This method returns this date in JDBC format.
+   *
+   * @return This date as a string.
+   *
+   * @deprecated
+   */
+  public String toString ()
+  {
+    return sdf.format (this);
+  }
+
 }
 
-public String toString() {
-	return sdf.format(this);
-}
-
-public int getYear() {
-	return (super.getYear());
-}
-
-public int getMonth() {
-	return (super.getMonth());
-}
-
-public int getDay() {
-	return (super.getDay());
-}
-
-public int getDate() {
-	return (super.getDate());
-}
-
-public void setYear(int i) {
-	super.setYear(i);
-}
-
-public void setMonth(int i) {
-	super.setMonth(i);
-}
-
-public void setDate(int i) {
-	super.setDate(i);
-}
-
-}
