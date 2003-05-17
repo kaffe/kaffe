af10d83b299c6eac1660a9d3f940a02d9f71c247
stack <stack>
2003-05-17 19:34:24 +0000

    remove xprof stuff, they were moved to xprofile-md.h

--- config/i386/freebsd2/md.h
+++ config/i386/freebsd2/md.h
@@ -44,33 +44,6 @@
 #define GET_SIGNAL_CONTEXT_POINTER(sc) (sc)
 #define SIGNAL_PC(scp) (scp)->sc_pc
 
-int kaffeStdProfRate(void);
-#define KAFFE_STD_PROF_RATE \
-int kaffeStdProfRate(void) \
-{ \
-	struct clockinfo clockinfo; \
-	int retval = 0; \
-	size_t size; \
-	int mib[2]; \
-	\
-	size = sizeof(clockinfo); \
-	mib[0] = CTL_KERN; \
-	mib[1] = KERN_CLOCKRATE; \
-	if (sysctl(mib, 2, &clockinfo, &size, NULL, 0) < 0) { \
-	} else if (clockinfo.profhz == 0) { \
-		retval = clockinfo.hz; \
-	} else { \
-		retval = clockinfo.profhz; \
-	} \
-	return( retval ); \
-}
-
-/* Override the standard mcount with the xprofiling one */
-#include <machine/profile.h>
-
-#define _KAFFE_OVERRIDE_MCOUNT_DEF _MCOUNT_DECL __P((uintfptr_t frompc, uintfptr_t selfpc))
-#define _KAFFE_OVERRIDE_MCOUNT MCOUNT
-
 #if defined(TRANSLATOR)
 #include "jit-md.h"
 #endif
