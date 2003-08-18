81e75158ea5f4f20b25cbfca2518a1b546ab769c
dalibor <dalibor>
2003-08-18 17:40:39 +0000

    Merged in RMI from GNU Classpath, replacing kaffe's old implementation. I've left out the RMISecurityManager for now, as it prevents native libraries needed for java.net from loading. I've changed classpath's implementation to fix a few bugs, and added kjc and jikes rmic compiler classes. Now at least RMIC works, but there are still some issues with serialization and class loading.

--- libraries/javalib/java/rmi/Naming.java
+++ libraries/javalib/java/rmi/Naming.java
@@ -1,10 +1,39 @@
 /*
- * Copyright (c) 1996, 1997, 1998, 1999
- *      Transvirtual Technologies, Inc.  All rights reserved.
- *
- * See the file "license-lesser.terms" for information on usage and 
- * redistribution of this file.
- */
+  Copyright (c) 1996, 1997, 1998, 1999 Free Software Foundation, Inc.
+
+This file is part of GNU Classpath.
+
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+Linking this library statically or dynamically with other modules is
+making a combined work based on this library.  Thus, the terms and
+conditions of the GNU General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module.  An independent module is a module which is not derived from
+or based on this library.  If you modify this library, you may extend
+this exception to your version of the library, but you are not
+obligated to do so.  If you do not wish to do so, delete this
+exception statement from your version. */
 
 package java.rmi;
 
@@ -15,26 +44,72 @@ import java.rmi.registry.LocateRegistry;
 
 public final class Naming {
 
+/** <pre>
+ * Looks for the remote object that is associated with the named service.
+ * Name and location is given in form of a URL without a scheme:
+ * 
+ *   //host:port/service-name
+ *  
+ * The port is optional.
+ * </pre>
+ * @param name the service name and location
+ * @return Remote-object that implements the named service
+ * @throws NotBoundException if no object implements the service
+ * @throws MalformedURLException 
+ * @throws RemoteException
+ */
 public static Remote lookup(String name) throws NotBoundException, MalformedURLException, RemoteException {
+	// hack to accept "rmi://host:port/service" strings
+	if(name.startsWith("rmi:")){ name = name.substring(4); }
 	URL u = new URL("http:" + name);
 	return (getRegistry(u).lookup(u.getFile().substring(1)));
 }
 
+/**
+ * Try to bind the given object to the given service name. 
+ * @param name
+ * @param obj
+ * @throws AlreadyBoundException
+ * @throws MalformedURLException
+ * @throws RemoteException
+ */
 public static void bind(String name, Remote obj) throws AlreadyBoundException, MalformedURLException, RemoteException {
 	URL u = new URL("http:" + name);
 	getRegistry(u).bind(u.getFile().substring(1), obj);
 }
 
+/**
+ * Remove a binding for a given service name.
+ * @param name
+ * @throws RemoteException
+ * @throws NotBoundException
+ * @throws MalformedURLException
+ */
 public static void unbind(String name) throws RemoteException, NotBoundException, MalformedURLException {
 	URL u = new URL("http:" + name);
 	getRegistry(u).unbind(u.getFile().substring(1));
 }
 
+/**
+ * Forces the binding between the given Remote-object and the given service name, even 
+ * if there was already an object bound to this name. 
+ * @param name
+ * @param obj
+ * @throws RemoteException
+ * @throws MalformedURLException
+ */
 public static void rebind(String name, Remote obj) throws RemoteException, MalformedURLException {
 	URL u = new URL("http:" + name);
 	getRegistry(u).rebind(u.getFile().substring(1), obj);
 }
 
+/**
+ * Lists all services at the named registry.
+ * @param name url that specifies the registry
+ * @return list of services at the name registry
+ * @throws RemoteException
+ * @throws MalformedURLException
+ */
 public static String[] list(String name) throws RemoteException, MalformedURLException {
 	return (getRegistry(new URL("http:" + name)).list());
 }
