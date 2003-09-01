77d806cfa41473ad82ad7f38c6ab0db2b70e58e4
guilhem <guilhem>
2003-09-01 05:51:24 +0000

    Added missing test case.

---
+++ test/regression/DeleteFile.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class DeleteFile
+{
+	public static void main(String args[]) throws Exception
+	{
+		FileOutputStream of = new FileOutputStream("deleteOnExit.test");
+		of.close();
+
+		File f = new File((File)null, "deleteOnExit.test");
+		f.deleteOnExit();
+
+		System.out.println("done.");
+		System.exit(0);
+	}
+}
+// java args: || test -f deleteOnExit.test
+/* Expected Output:
+done.
+*/
