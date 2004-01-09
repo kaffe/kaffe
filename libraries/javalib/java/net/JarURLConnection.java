8eef13c411c724402dfe53cfe21e381640e70a45
dalibor <dalibor>
2004-01-09 16:45:58 +0000

    Resynced with GNU Classpath
    2004-01-08  Dalibor Topic <robilad@kaffe.org>
    
            Resynced with GNU Classpath.
    
            2004-01-06  Michael Koch  <konqueror@gmx.de>
    
            * java/net/JarURLConnection.java
            (JarURLConnection): Initialize entryName with null if no entry
            present, fixed off by one error.

--- libraries/javalib/java/net/JarURLConnection.java
+++ libraries/javalib/java/net/JarURLConnection.java
@@ -79,6 +79,13 @@ public abstract class JarURLConnection extends URLConnection
   private final URL jarFileURL;
 
   /**
+   * The connection to the jar file itself. A JarURLConnection
+   * can represent an entry in a jar file or an entire jar file.  In
+   * either case this describes just the jar file itself.
+   */
+  protected URLConnection jarFileURLConnection;
+
+  /**
    * This is the jar file "entry name" or portion after the "!/" in the
    * URL which represents the pathname inside the actual jar file.
    */
@@ -110,7 +117,7 @@ public abstract class JarURLConnection extends URLConnection
     jarFileURL = new URL (spec.substring (0, bang));
 
     // Get the name of the entry, if any.
-    entryName = spec.length() == (bang + 1) ? "" : spec.substring (bang + 2);
+    entryName = spec.length() == (bang + 2) ? null : spec.substring (bang + 2);
   }
 
   /**
