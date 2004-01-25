10e7c6f16b2e26e1505237811ec0980210fd0b11
guilhem <guilhem>
2004-01-25 17:41:23 +0000

    New compile_time regression test.

---
+++ test/regression/compile_time/VarInit.java
@@ -0,0 +1,5 @@
+public interface VarInit
+{
+	int c = VarInit2.BASE_INT + 1;
+	int a = 2 + c;
+}
---
+++ test/regression/compile_time/VarInit2.java
@@ -0,0 +1,14 @@
+public class VarInit2
+{
+	static final int BASE_INT = 10;
+
+	public static void a(int b)
+	{
+		switch (b)
+		{
+			case VarInit.c:
+			default:
+				break;
+		}
+	}
+}
