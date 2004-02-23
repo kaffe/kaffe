f80332d2cb21bf354a6d55908da12fe7de3cfe69
dalibor <dalibor>
2004-02-23 22:20:28 +0000

    Resynced with GNU Classpath
    2004-02-23  Dalibor Topic <robilad@kaffe.org>
    
            Resynced with GNU Classpath
    
            2004-02-06  Mohan Embar  <gnustuff@thisiscool.com>
    
            * gnu/java/net/PlainSocketImpl.java
            (inChannelOperation): New field.
            (isInChannelOperation): New accessor.
            (setInChannelOperation): New modifier.

--- libraries/javalib/gnu/java/net/PlainSocketImpl.java
+++ libraries/javalib/gnu/java/net/PlainSocketImpl.java
@@ -29,7 +29,33 @@ private boolean closed;
 private boolean blocking;
 private boolean connecting;
 private int native_fd;
+  /**
+   * Indicates whether a channel initiated whatever operation
+   * is being invoked on this socket.
+   */
+  private boolean inChannelOperation;
 
+  /**
+   * Indicates whether we should ignore whether any associated
+   * channel is set to non-blocking mode. Certain operations
+   * throw an <code>IllegalBlockingModeException</code> if the
+   * associated channel is in non-blocking mode, <i>except</i>
+   * if the operation is invoked by the channel itself.
+   */
+  public final boolean isInChannelOperation()
+  {
+    return inChannelOperation;
+  }
+  
+  /**
+   * Sets our indicator of whether an I/O operation is being
+   * initiated by a channel.
+   */
+  public final void setInChannelOperation(boolean b)
+  {
+    inChannelOperation = b;
+  }
+ 
 static {
 	System.loadLibrary("net");
 }
