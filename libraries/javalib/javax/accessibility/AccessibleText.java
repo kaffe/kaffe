d715919d070449065f68cb47476fc20f2fe8ca11
dalibor <dalibor>
2004-04-19 15:23:51 +0000

    Resynced javax/accessibility/AccessibleText.java with GNU Classpath
    2004-04-19  Dalibor Topic <robilad@kaffe.org>
    
            * libraries/javalib/javax/accessibility/AccessibleText.java:
            Resynced with GNU Classpath.
    
            2004-04-19  Dalibor Topic  <robilad@kaffe.org>
    
            * javax/accessibility/AccessibleText.java:
            Cleaned up imports.

--- libraries/javalib/javax/accessibility/AccessibleText.java
+++ libraries/javalib/javax/accessibility/AccessibleText.java
@@ -1,5 +1,5 @@
 /* AccessibleText.java -- aids in accessibly manipulating text
-   Copyright (C) 2000, 2002 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2002, 2004 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
