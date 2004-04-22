857991965d2502e0963ea9f8a450993efe78b9b3
dalibor <dalibor>
2004-04-22 16:20:55 +0000

    Resynced java.sql with GNU Classpath
    2004-04-22  Dalibor Topic <robilad@kaffe.org>
    
            * libraries/javalib/java/sql/Array.java,
            libraries/javalib/java/sql/DatabaseMetaData.java,
            libraries/javalib/java/sql/DriverManager.java,
            libraries/javalib/java/sql/ResultSet.java:
            Resynced with GNU Classpath.
    
            2004-04-22 Dalibor Topic <robilad@kaffe.org>
    
            * java/sql/DriverManager.java:
            Cleaned up imports.
    
            2004-04-18  Michael Koch  <konqueror@gmx.de>
    
            * java/sql/Array.java,
            java/sql/DatabaseMetaData.java,
            java/sql/ResultSet.java:
            Fixed HTML tags in javadocs.

--- libraries/javalib/java/sql/DriverManager.java
+++ libraries/javalib/java/sql/DriverManager.java
@@ -1,5 +1,5 @@
 /* DriverManager.java -- Manage JDBC drivers
-   Copyright (C) 1999, 2000, 2001, 2003 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2000, 2001, 2003, 2004 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
