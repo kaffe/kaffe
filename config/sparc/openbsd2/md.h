2bdfca5d8a6e9e1bf9a4d1878bcaa2b3ba8fe0f7
guilhem <guilhem>
2004-04-25 13:29:21 +0000

    Added a missing include on openbsd/sparc.
            * config/sparc/openbsd2/md.h: Added #include <sys/time.h>
            because sys/resource.h has an undefined struct on openbsd.

--- config/sparc/openbsd2/md.h
+++ config/sparc/openbsd2/md.h
@@ -14,6 +14,9 @@
 
 #include "sparc/common.h"
 #include "sparc/threads.h"
+#if defined(HAVE_SYS_TIME_H)
+#include <sys/time.h>
+#endif
 #if defined(HAVE_SYS_RESOURCE_H)
 #include <sys/resource.h>
 #endif
