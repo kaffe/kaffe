e3bcb38d6318db1bbc65945ea9630b76691e42ab
dalibor <dalibor>
2004-07-13 14:17:04 +0000

    Fix 'use of compound as lvalue is deprecated' warnings for jit
    2004-07-13  Kiyo Inaba <inaba@src.ricoh.co.jp>
    
           * kaffe/kaffevm/jit/funcs.c
           * config/i386/jit-i386.def:
           * config/i386/jit3-i386.def:
           * config/m68k/jit-i386.def:
           * config/m68k/jit3-i386.def:
           Fix 'use of compound as lvalue is deprecated' warnings for jit.

--- kaffe/kaffevm/jit/funcs.c
+++ kaffe/kaffevm/jit/funcs.c
@@ -8,7 +8,6 @@
  */
 
 #include "config.h"
-#include "debug.h"
 #include "config-std.h"
 #include "gtypes.h"
 #include "seq.h"
@@ -20,14 +19,14 @@
 #include "errors.h"
 #include "machine.h"
 #include "md.h"
+#include "debug.h"
 
 /* XXX: HUGE name clash: machine.h defines a define_insn for use in 
  * kaffe.def.  The define_insn below, however, is used in jit.def
  *
  * We must rename one or the other!
  */
-#undef define_insn
-
+#undef	define_insn
 #define	define_insn(n, i) void i (sequence* s)
 
 #define ALIGN(byte)							\
@@ -35,11 +34,34 @@
 		   ? CODEPC + (byte) - (CODEPC % (byte))		\
 		   : CODEPC))
 
-#define	OUT	(codeblock[CODEPC++])
-#define	BOUT	(*(uint8*)&codeblock[CODEPC++])
-#define	WOUT	(*(uint16*)&codeblock[(CODEPC += 2) - 2])
-#define	LOUT	(*(uint32*)&codeblock[(CODEPC += 4) - 4])
-#define	QOUT	(*(uint64*)&codeblock[(CODEPC += 8) - 8])
+#if defined(KAFFE_VMDEBUG)
+/*
+ * Print any labels that refer to the current address.
+ */
+static
+void printCodeLabels(void)
+{
+	label *il, *curr = 0;
+
+	while( (il = getInternalLabel(&curr, CODEPC)) )
+	{
+		kaffe_dprintf("%s:\n", il->name);
+	}
+}
+#undef OUT
+#define	OUT(v)	do { DBGEXPR(JIT,(void)printCodeLabels(),0),codeblock[CODEPC] = v; CODEPC++; } while (0)
+#define	BOUT(v)	do { DBGEXPR(JIT,(void)printCodeLabels(),0),*(uint8*)&codeblock[CODEPC] = v; CODEPC++; } while (0)
+#define	WOUT(v)	do { DBGEXPR(JIT,(void)printCodeLabels(),0),*(uint16*)&codeblock[CODEPC] = v; CODEPC += 2; } while (0)
+#define	LOUT(v)	do { DBGEXPR(JIT,(void)printCodeLabels(),0),*(uint32*)&codeblock[CODEPC] = v; CODEPC += 4; } while (0)
+#define	QOUT(v)	do { DBGEXPR(JIT,(void)printCodeLabels(),0),*(uint64*)&codeblock[CODEPC] = v; CODEPC += 8; } while (0)
+#else
+#undef OUT
+#define	OUT(v)	do { codeblock[CODEPC] = v; CODEPC++; } while (0)
+#define	BOUT(v)	do { *(uint8*)&codeblock[CODEPC] = v; CODEPC++; } while (0)
+#define	WOUT(v)	do { *(uint16*)&codeblock[CODEPC] = v; CODEPC += 2; } while (0)
+#define	LOUT(v)	do { *(uint32*)&codeblock[CODEPC] = v; CODEPC += 4; } while (0)
+#define	QOUT(v)	do { *(uint64*)&codeblock[CODEPC] = v; CODEPC += 8; } while (0)
+#endif
 
 #include "jit.def"
 #include "trampolines.c"
