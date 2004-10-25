d313a4a647ee9523a621111b7bcb2ccefe9c237e
robilad <robilad>
2004-10-25 17:18:01 +0000

    Resynced with GNU Classpath: Implemented LoginContext
    2004-10-25  Dalibor Topic  <robilad@kaffe.org>
    
            * libraries/javalib/gnu/java/security/action/GetSecurityPropertyAction.java:
            New file taken from GNU Classpath.
    
            * libraries/javalib/Makefile.am,
            libraries/javalib/Makefile.in,
            libraries/javalib/all.files:
            Regenerated.
    
            * libraries/javalib/javax/security/auth/login/Configuration.java,
            libraries/javalib/javax/security/auth/login/LoginContext.java,
            libraries/javalib/javax/security/auth/spi/LoginModule.java:
            Resynced with GNU Classpath.
    
            2004-10-24  Casey Marshall  <csm@gnu.org>
    
            * gnu/java/security/action/GetSecurityPropertyAction.java: new file.
            * javax/security/auth/login/Configuration.java
            (getConfig): new method.
            * javax/security/auth/login/LoginContext.java: implemented.
            * javax/security/auth/spi/LoginModule.java: new file.

--- libraries/javalib/javax/security/auth/spi/LoginModule.java
+++ libraries/javalib/javax/security/auth/spi/LoginModule.java
@@ -1,25 +1,22 @@
-/* LoginModule.java
-   Copyright (C) 2004 Free Software Foundation, Inc.
+/* LoginModule.java -- interface for login implementations.
+   Copyright (C) 2004  Free Software Foundation, Inc.
 
-This file is part of GNU Crypto.
+This file is part of GNU Classpath.
 
-GNU Crypto is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 2, or (at your option) any
-later version.
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
 
-GNU Crypto is distributed in the hope that it will be useful, but
+GNU Classpath is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; see the file COPYING.  If not, write to the
-
-   Free Software Foundation Inc.,
-   59 Temple Place - Suite 330,
-   Boston, MA 02111-1307
-   USA
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
 
 Linking this library statically or dynamically with other modules is
 making a combined work based on this library.  Thus, the terms and
@@ -36,66 +33,89 @@ module.  An independent module is a module which is not derived from
 or based on this library.  If you modify this library, you may extend
 this exception to your version of the library, but you are not
 obligated to do so.  If you do not wish to do so, delete this
-exception statement from your version.  */
+exception statement from your version. */
 
 
 package javax.security.auth.spi;
 
 import java.util.Map;
+
 import javax.security.auth.Subject;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.login.LoginException;
 
 /**
+ * The base interface for login methods in the Java Authentication and
+ * Authorization Service (JAAS).
  *
+ * <p>This interface is used by service providers that implement login
+ * services, and is used internally by the JAAS system. It is not useful
+ * to application programmers, who should use the {@link
+ * javax.security.auth.login.LoginContext} instead.
+ *
+ * @author Casey Marshall (csm@gnu.org)
  */
 public interface LoginModule
 {
-
   /**
-   * Aborts the current login attempt.
+   * Abort the current login attempt. This is called after {@link #login()}
+   * if the overall login attempt fails (that is, if one of the other login
+   * modules that is REQUIRED or REQUISITE fails). This method should clean
+   * up this module's saved state, if any.
    *
-   * @return True if this method succeeds, or false if this module should be
-   *         ignored.
-   * @throws LoginException If this method fails.
+   * @return True if the abort succeeded, or false if this module should
+   * be ignored.
+   * @throws LoginException If the abort fails.
    */
   boolean abort() throws LoginException;
 
   /**
-   * Commits (finishes) a login attempt.
+   * Commit the current login attempt. This is called after {@link
+   * #login()} if the overall login attempt succeeds (that is, all
+   * methods have satisfied all REQUIRED, REQUISITE, SUFFICIENT and
+   * OPTIONAL module requirements).
    *
-   * @return True if this method succeeds, or false if this module should be
-   *         ignored.
-   * @throws LoginException If this method fails.
+   * @return True if the commit succeeded, or false if this module
+   * should be ignored.
+   * @throws LoginException If the commit fails.
    */
   boolean commit() throws LoginException;
 
   /**
-   * Initializes this login module with a subject, a callback handler to
-   * handle input, a state shared amongst modules, and options.
+   * Initializes this login module. This method is called when the
+   * instance implementing this interface is instantiated, and should
+   * perform any initialization based on the given parameters.
+   * Implementations should ignore state variables and options they do
+   * not recognize.
    *
    * @param subject The subject being authenticated.
-   * @param handler The callback handler that will handle user input.
-   * @param sharedState The state shared between modules.
-   * @param options The options for this module.
+   * @param handler The callback handler for user input.
+   * @param sharedState A mapping that is shared between all login
+   * modules.
+   * @param options A mapping of options given to this module.
    */
-  void initialize (Subject subject, CallbackHandler handler, Map sharedState,
-                   Map options);
+  void initialize(Subject subject, CallbackHandler handler,
+                  Map sharedState, Map options);
 
   /**
-   * Begin a login attempt with this module.
+   * Authenticates a subject to the system. This is the primary
+   * mechanism by which subjects are authenticated, and typically
+   * implementations will ask for credentials (for example, a user
+   * name and password) which will then be verified.
    *
-   * @return True if this method succeeds, or false if this module should be
-   *         ignored.
+   * @return True if the subject was authenticated, or false if this
+   * module should be ignored.
    * @throws LoginException If this method fails.
    */
   boolean login() throws LoginException;
 
   /**
-   * Log a user out of this module.
+   * Logs a subject out. This is primarily used for modules that must
+   * destroy or remove the authentication state associated with a
+   * logged-in subject.
    *
-   * @return True if this method succeeds, or false if this module should be
-   *         ignored.
+   * @return True if the logout succeeds, or false if this module
+   * should be ignored.
    * @throws LoginException If this method fails.
    */
   boolean logout() throws LoginException;
