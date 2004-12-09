7f2c8519ef1ecf2be3248cb6e04b544c79723c58
doogie <doogie>
2004-12-09 05:19:32 +0000

    Add call to enableXProfiling.

--- kaffe/kaffe/main.c
+++ kaffe/kaffe/main.c
@@ -153,7 +153,7 @@ main(int argc, char* argv[])
 #if defined(KAFFE_XPROFILER)
 	if( xProfFlag )
 	{
-		if( !enableXCallGraph() )
+		if( !enableXCallGraph() && !enableXProfiling() )
 		{
 			fprintf(stderr, 
 				"Unable to initialize cross "
