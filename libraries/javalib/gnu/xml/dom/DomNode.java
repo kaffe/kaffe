8ce88547da54e9c5ad377022f5d6e830c8bab040
robilad <robilad>
2005-01-29 15:46:01 +0000

    Resynced with GNU Classpath: dom node fix
    2005-01-29  Dalibor Topic  <robilad@kaffe.org>
    
            Resynced with GNU Classpath.
    
    2005-01-28  Chris Burdess  <dog@gnu.org>
    
    * gnu/xml/DomNode: Correct implementation of getElementsByTagName
    etc to return a NodeList that returns consistent results from item
    and getLength methods.

--- libraries/javalib/gnu/xml/dom/DomNode.java
+++ libraries/javalib/gnu/xml/dom/DomNode.java
@@ -1347,13 +1347,17 @@ public abstract class DomNode
           current.previousNode ();
           lastIndex--;
         }
-        return current.previousNode ();
+        Node ret = current.previousNode ();
+        current = null;
+        return ret;
       } 
       
       // somewhere after last node
       while (++lastIndex != index)
         current.nextNode ();
-        return current.nextNode ();
+        Node ret = current.nextNode ();
+        current = null;
+        return ret;
     }
     
     public int getLength()
@@ -1365,6 +1369,7 @@ public abstract class DomNode
         {
           retval++;
         }
+      current = null;
       return retval;
     }
     
