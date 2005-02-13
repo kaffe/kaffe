dd5aefc2d931d19eb7f17e43016b7118a0f3196a
robilad <robilad>
2005-02-13 22:26:46 +0000

    Resynced with GNU Classpath: collator workaround for gjdoc
    2005-02-13  Dalibor Topic  <robilad@kaffe.org>
    
            Resynced with GNU Classpath.
    
            2005-02-10  Jeroen Frijters  <jeroen@frijters.net>
    
            * java/text/Collator.java (getInstance(Locale)):
            Added default collation pattern to handle case when resource
            is missing and throw InternalError instead of returning null
            should parsing fail.

--- libraries/javalib/java/text/Collator.java
+++ libraries/javalib/java/text/Collator.java
@@ -308,7 +308,8 @@ public abstract class Collator implements Comparator, Cloneable
       }
     catch (MissingResourceException x)
       {
-	return null;
+	pattern = "<0<1<2<3<4<5<6<7<8<9<A,a<b,B<c,C<d,D<e,E<f,F<g,G<h,H<i,I<j,J<k,K" +
+		"<l,L<m,M<n,N<o,O<p,P<q,Q<r,R<s,S<t,T<u,U<v,V<w,W<x,X<y,Y<z,Z";
       }
     try
       {
@@ -316,7 +317,7 @@ public abstract class Collator implements Comparator, Cloneable
       }
     catch (ParseException x)
       {
-	return null;
+	throw (InternalError)new InternalError().initCause(x);
       }
   }
 
