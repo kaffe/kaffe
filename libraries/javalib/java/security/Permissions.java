085db73080142219e74c2413a36d710d749691ba
robilad <robilad>
2005-02-19 16:10:22 +0000

    Resynced with GNU Classpath: permission fixes
    2005-02-19  Dalibor Topic  <robilad@kaffe.org>
    
    Resynced with GNU Classpath.
    
    2005-02-16  Andrew Haley  <aph@redhat.com>
    
    * javax/security/auth/Subject.java (doAsPrivileged): If acc is
    null, create a new AccessControlContext.
    * java/security/SecureClassLoader.java (protectionDomainCache):
    new field.
    (defineClass): Create a new protection domain and add it to our
    cache.
    
    * java/rmi/server/UnicastRemoteObject.java (exportObject): Call
    addStub() to keep track of the stub we've exported.
    (unexportObject): Call deleteStub().
    * java/rmi/server/RemoteObject.java (stubs): New field.
    (addStub): New method.
    (deleteStub): New method.
    (toStub): Rewrite.
    
    * java/security/Permissions.java (PermissionsHash.implies):
    Iterate over the collection and invoke implies() on each
    element.

--- libraries/javalib/java/security/Permissions.java
+++ libraries/javalib/java/security/Permissions.java
@@ -227,9 +227,18 @@ public final class Permissions extends PermissionCollection
      * @param perm the permission to check
      * @return true if it is implied
      */
+    // FIXME: Should this method be synchronized?
     public boolean implies(Permission perm)
     {
-      return perms.get(perm) != null;
+      Enumeration elements = elements();
+      
+      while (elements.hasMoreElements())
+	{
+	  Permission p = (Permission)elements.nextElement();
+	  if (p.implies(perm))
+	    return true;
+	}
+      return false;
     }
 
     /**
