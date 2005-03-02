678ddd9e90458cd794fb25136f778c586a9d6f62
robilad <robilad>
2005-03-02 22:27:45 +0000

    Resynced with GNU Classpath: swing fixlet
    2005-03-02  Dalibor Topic  <robilad@kaffe.org>
    
            Resynced with GNU Classpath.
    
            2005-02-26  Audrius Meskauskas  <audriusa@bluewin.ch>
    
            * javax/swing/Timer.java: documenting and some
              formatting.

--- libraries/javalib/javax/swing/Timer.java
+++ libraries/javalib/javax/swing/Timer.java
@@ -1,5 +1,5 @@
 /* Timer.java --
-   Copyright (C) 2002, 2004  Free Software Foundation, Inc.
+   Copyright (C) 2002, 2004, 2005  Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -46,33 +46,70 @@ import java.util.EventListener;
 import javax.swing.event.EventListenerList;
 
 /**
- * DOCUMENT ME!
+ * Fires one or more action events after the specified delay.
+ * @author Ronald Veldema
+ * @author Audrius Meskauskas (audriusa@Bionformatics.org) - bug fixes
+ * and documentation comments
  */
-public class Timer implements Serializable
+public class Timer
+  implements Serializable
 {
-  /** DOCUMENT ME! */
+  /**
+   * The timer thread
+   */
+  private class Waker
+    extends Thread
+  {
+    /**
+     * Fires events, pausing for required intervals.
+     */
+    public void run()
+    {
+      running = true;
+      try
+        {
+          sleep(initialDelay);
+
+          queueEvent();
+
+          while (running)
+            {
+              try
+                {
+                  sleep(delay);
+                }
+              catch (InterruptedException e)
+                {
+                  return;
+                }
+              queueEvent();
+
+              if (logTimers)
+                System.out.println("javax.swing.Timer -> clocktick");
+
+              if ( ! repeats)
+                break;
+            }
+          running = false;
+        }
+      catch (Exception e)
+        {
+          // The timer is no longer running.
+          running = false;
+        }
+    }
+  }
+
+  /**
+   * Use serialVersionUID for interoperability.
+   */
   private static final long serialVersionUID = -1116180831621385484L;
 
-  /** DOCUMENT ME! */
-  protected EventListenerList listenerList = new EventListenerList();
-
-  // This object manages a "queue" of virtual actionEvents, maintained as a
-  // simple long counter. When the timer expires, a new event is queued,
-  // and a dispatcher object is pushed into the system event queue. When
-  // the system thread runs the dispatcher, it will fire as many
-  // ActionEvents as have been queued, unless the timer is set to
-  // coalescing mode, in which case it will fire only one ActionEvent.
-
-  /** DOCUMENT ME! */
-  private long queue;
-
-  /** DOCUMENT ME! */
-  private Object queueLock = new Object();
-
-  /** DOCUMENT ME! */
-  private Waker waker;
-
-  private Runnable drainer = new Runnable() 
+  /**
+   * The encloding class, used with {@link SwingUtilities#invokeLater}
+   * to invoke the {@link #drainEvents()}.
+   */
+  private Runnable drainer = new Runnable()
     {
       public void run()
       {
@@ -81,105 +118,70 @@ public class Timer implements Serializable
     };
 
   /**
-   * DOCUMENT ME!
+   * If <code>true</code>, the timer prints a message to
+   * {@link System#out} when firing each event.
    */
-  private void queueEvent()
-  {
-    synchronized (queueLock)
-      {
-	queue++;
-	if (queue == 1)
-	  SwingUtilities.invokeLater(drainer);
-      }
-  }
-
-  /**
-   * DOCUMENT ME!
-   */
-  private void drainEvents()
-  {
-    synchronized (queueLock)
-      {
-	if (isCoalesce())
-	  {
-	    if (queue > 0)
-	      fireActionPerformed();
-	  }
-	else
-	  {
-	    while (queue > 0)
-	      {
-		fireActionPerformed();
-		queue--;
-	      }
-	  }
-	queue = 0;
-      }
-  }
-
   static boolean logTimers;
 
-  /** DOCUMENT ME! */
+  /**
+   * A field to store all listeners who are listening to this timer.
+   */
+  protected EventListenerList listenerList = new EventListenerList();
+
+  /**
+   * <code>true</code> if the timer coalesces events.
+   */
   boolean coalesce = true;
 
-  /** DOCUMENT ME! */
+  /**
+   * <code>true</code> if the timer is firing repetetive events.
+   */
   boolean repeats = true;
 
-  /** DOCUMENT ME! */
+  /**
+   * <code>true</code> if the timer is currently active, firing events
+   * as scheduled.
+   */
   boolean running;
 
-  /** DOCUMENT ME! */
-  int ticks;
-
-  /** DOCUMENT ME! */
+  /**
+   * The delay between subsequent repetetive events.
+   */
   int delay;
 
-  /** DOCUMENT ME! */
+  /**
+   * The initial delay before the first event.
+   */
   int initialDelay;
 
   /**
-   * DOCUMENT ME!
+   * The number of events that have been already fired by this timer.
+   * This is used as a numeric identifier for the next event that would
+   * be fired.
    */
-  private class Waker extends Thread
-  {
-    /**
-     * DOCUMENT ME!
-     */
-    public void run()
-    {
-      running = true;
-      try
-        {
-	  sleep(initialDelay);
-	  
-	  queueEvent();
+  int ticks;
 
-	  while (running)
-	    {
-	      try
-	        {
-		  sleep(delay);
-	        }
-	      catch (InterruptedException e)
-	        {
-		  return;
-	        }
-	      queueEvent();
+  /**
+   * Stores the thread that posts events to the queue at required time
+   * intervals.
+   */
+  private Waker waker;
 
-	      if (logTimers)
-		System.out.println("javax.swing.Timer -> clocktick");
+  /**
+   * This object manages a "queue" of virtual actionEvents, maintained as a
+   * simple long counter. When the timer expires, a new event is queued,
+   * and a dispatcher object is pushed into the system event queue. When
+   * the system thread runs the dispatcher, it will fire as many
+   * ActionEvents as have been queued, unless the timer is set to
+   * coalescing mode, in which case it will fire only one ActionEvent.
+   */
+  private long queue;
 
-	      if (! repeats)
-		break;
-	    }
-	  running = false;
-        }
-      catch (Exception e)
-        {
-//	  System.out.println("swing.Timer::" + e);
-        }
-    }
-  }
+  /**
+   * <code>synchronized(queueLock)</code> replaces
+   * <code>synchronized(queue)</code> that is not supported by this language.
+   */
+  private Object queueLock = new Object();
 
   /**
    * Creates a new Timer object.
@@ -191,70 +193,17 @@ public class Timer implements Serializable
   public Timer(int d, ActionListener listener)
   {
     delay = d;
-	initialDelay = d;
+    initialDelay = d;
 
     if (listener != null)
       addActionListener(listener);
   }
 
   /**
-   * DOCUMENT ME!
+   * Get the array of action listeners.
    *
-   * @param c DOCUMENT ME!
-   */
-  public void setCoalesce(boolean c)
-  {
-    coalesce = c;
-  }
-
-  /**
-   * DOCUMENT ME!
-   *
-   * @return DOCUMENT ME!
-   */
-  public boolean isCoalesce()
-  {
-    return coalesce;
-  }
-
-  /**
-   * DOCUMENT ME!
-   *
-   * @param listener DOCUMENT ME!
-   */
-  public void addActionListener(ActionListener listener)
-  {
-    listenerList.add(ActionListener.class, listener);
-  }
-
-  /**
-   * DOCUMENT ME!
-   *
-   * @param listener DOCUMENT ME!
-   */
-  public void removeActionListener(ActionListener listener)
-  {
-    listenerList.remove(ActionListener.class, listener);
-  }
-
-  /**
-   * DOCUMENT ME!
-   *
-   * @param listenerType DOCUMENT ME!
-   *
-   * @return DOCUMENT ME!
-   *
-   * @since 1.3
-   */
-  public EventListener[] getListeners(Class listenerType)
-  {
-    return listenerList.getListeners(listenerType);
-  }
-
-  /**
-   * DOCUMENT ME!
-   *
-   * @return DOCUMENT ME!
+   * @return the array of action listeners that are listening for the events,
+   * fired by this timer
    *
    * @since 1.4
    */
@@ -264,30 +213,55 @@ public class Timer implements Serializable
   }
 
   /**
-   * DOCUMENT ME!
+   * Sets whether the Timer coalesces multiple pending event firings.
+   * If the coalescing is enabled, the multiple events that have not been
+   * fired on time are replaced by the single event. The events may not
+   * be fired on time if the application is busy.
    *
-   * @param event DOCUMENT ME!
+   * @param c <code>true</code> (default) to enable the event coalescing,
+   * <code>false</code> otherwise
    */
-  protected void fireActionPerformed(ActionEvent event)
+  public void setCoalesce(boolean c)
   {
-    ActionListener[] listeners = getActionListeners();
-
-    for (int i = 0; i < listeners.length; i++)
-      listeners[i].actionPerformed(event);
+    coalesce = c;
   }
 
   /**
-   * DOCUMENT ME!
+   * Checks if the Timer coalesces multiple pending event firings.
+   * If the coalescing is enabled, the multiple events that have not been
+   * fired on time are replaced by the single event. The events may not
+   * be fired on time if the application is busy.
+   *
+   * @return <code>true</code> if the coalescing is enabled,
+   * <code>false</code> otherwise
    */
-  void fireActionPerformed()
+  public boolean isCoalesce()
   {
-    fireActionPerformed(new ActionEvent(this, ticks++, "Timer"));
+    return coalesce;
   }
 
   /**
-   * DOCUMENT ME!
+   * Get the event listeners of the given type that are listening for the
+   * events, fired by this timer.
    *
-   * @param lt DOCUMENT ME!
+   * @param listenerType the listener type (for example, ActionListener.class)
+   *
+   * @return the array of event listeners that are listening for the events,
+   * fired by this timer
+   * @since 1.3
+   */
+  public EventListener[] getListeners(Class listenerType)
+  {
+    return listenerList.getListeners(listenerType);
+  }
+
+  /**
+   * Set the timer logging state. If it is set to <code>true</code>, the
+   * timer prints a message to {@link System#out} when firing each
+   * action event.
+   *
+   * @param lt <code>true</code> if logging is enabled, <code>false</code>
+   * (default value) otherwise
    */
   public static void setLogTimers(boolean lt)
   {
@@ -295,9 +269,11 @@ public class Timer implements Serializable
   }
 
   /**
-   * DOCUMENT ME!
+   * Return the logging state.
    *
-   * @return DOCUMENT ME!
+   * @return <code>true</code> if the timer is printing a message to
+   * {@link System#out}
+   * when firing each action event
    */
   public static boolean getLogTimers()
   {
@@ -305,9 +281,11 @@ public class Timer implements Serializable
   }
 
   /**
-   * DOCUMENT ME!
+   * Set the delay between firing the subsequent events.
+   * This parameter does not change the value of the initial delay before
+   * firing the first event.
    *
-   * @param d DOCUMENT ME!
+   * @param d The time gap between the subsequent events, in milliseconds
    */
   public void setDelay(int d)
   {
@@ -315,9 +293,9 @@ public class Timer implements Serializable
   }
 
   /**
-   * DOCUMENT ME!
+   * Get the delay between firing the subsequent events.
    *
-   * @return DOCUMENT ME!
+   * @return The delay between subsequent events, in milliseconds
    */
   public int getDelay()
   {
@@ -325,9 +303,12 @@ public class Timer implements Serializable
   }
 
   /**
-   * DOCUMENT ME!
+   * Set the intial delay before firing the first event since calling
+   * the {@link #start()} method. If the initial delay has not been
+   * set, it is assumed having the same value as the delay between the
+   * subsequent events.
    *
-   * @param i DOCUMENT ME!
+   * @param i the initial delay, in milliseconds
    */
   public void setInitialDelay(int i)
   {
@@ -335,9 +316,11 @@ public class Timer implements Serializable
   }
 
   /**
-   * DOCUMENT ME!
+   * Get the intial delay before firing the first event since calling
+   * the {@link #start()} method. If the initial delay has not been
+   * set, returns the same value as {@link #getDelay()}.
    *
-   * @return DOCUMENT ME!
+   * @return the initial delay before firing the first action event.
    */
   public int getInitialDelay()
   {
@@ -345,9 +328,11 @@ public class Timer implements Serializable
   }
 
   /**
-   * DOCUMENT ME!
+   * Enable firing the repetetive events.
    *
-   * @param r DOCUMENT ME!
+   * @param r <code>true</code> (default value) to fire repetetive events.
+   * <code>false</code> to fire
+   * only one event after the initial delay
    */
   public void setRepeats(boolean r)
   {
@@ -355,9 +340,11 @@ public class Timer implements Serializable
   }
 
   /**
-   * DOCUMENT ME!
+   * Check is this timer fires repetetive events.
    *
-   * @return DOCUMENT ME!
+   * @return <code>true</code> if the timer fires repetetive events,
+   * <code>false</code> if it fires
+   * only one event after the initial delay
    */
   public boolean isRepeats()
   {
@@ -365,9 +352,11 @@ public class Timer implements Serializable
   }
 
   /**
-   * DOCUMENT ME!
+   * Get the timer state.
    *
-   * @return DOCUMENT ME!
+   * @return <code>true</code> if the timer has been started and is firing
+   * the action events as scheduled. <code>false</code>
+   * if the timer is inactive.
    */
   public boolean isRunning()
   {
@@ -375,7 +364,37 @@ public class Timer implements Serializable
   }
 
   /**
-   * DOCUMENT ME!
+   * Add the action listener
+   *
+   * @param listener the action listener to add
+   */
+  public void addActionListener(ActionListener listener)
+  {
+    listenerList.add(ActionListener.class, listener);
+  }
+
+  /**
+   * Remove the action listener.
+   *
+   * @param listener the action listener to remove
+   */
+  public void removeActionListener(ActionListener listener)
+  {
+    listenerList.remove(ActionListener.class, listener);
+  }
+
+  /**
+   * Cancel all pending tasks and fire the first event after the initial
+   * delay.
+   */
+  public void restart()
+  {
+    stop();
+    start();
+  }
+
+  /**
+   * Start firing the action events.
    */
   public void start()
   {
@@ -386,16 +405,7 @@ public class Timer implements Serializable
   }
 
   /**
-   * DOCUMENT ME!
-   */
-  public void restart()
-  {
-    stop();
-    start();
-  }
-
-  /**
-   * DOCUMENT ME!
+   * Stop firing the action events.
    */
   public void stop()
   {
@@ -404,7 +414,70 @@ public class Timer implements Serializable
       waker.interrupt();
     synchronized (queueLock)
       {
-	queue = 0;
+        queue = 0;
+      }
+  }
+
+  /**
+   * Fire the given action event to the action listeners.
+   *
+   * @param event the event to fire
+   */
+  protected void fireActionPerformed(ActionEvent event)
+  {
+    ActionListener[] listeners = getActionListeners();
+
+    for (int i = 0; i < listeners.length; i++)
+      listeners [ i ].actionPerformed(event);
+  }
+
+  /**
+   * Fire the action event, named "Timer" and having the numeric
+   * identifier, equal to the numer of events that have been
+   * already fired before.
+   */
+  void fireActionPerformed()
+  {
+    fireActionPerformed(new ActionEvent(this, ticks++, "Timer"));
+  }
+
+  /**
+   * Fire the queued action events.
+   * In the coalescing mode, a single event is fired as a replacement
+   * for all queued events. In non coalescing mode, a series of
+   * all queued events is fired.
+   */
+  private void drainEvents()
+  {
+    synchronized (queueLock)
+      {
+        if (isCoalesce())
+          {
+            if (queue > 0)
+              fireActionPerformed();
+          }
+        else
+          {
+            while (queue > 0)
+              {
+                fireActionPerformed();
+                queue--;
+              }
+          }
+        queue = 0;
+      }
+  }
+
+  /**
+  * Post a scheduled event to the event queue.
+  */
+  private void queueEvent()
+  {
+    synchronized (queueLock)
+      {
+        queue++;
+        if (queue == 1)
+          SwingUtilities.invokeLater(drainer);
       }
   }
 }
