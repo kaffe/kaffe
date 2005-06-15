19c16638dbdca64e591b25393993469f0642d78e
guilhem <guilhem>
2005-06-15 09:56:29 +0000

    Bug fix for FPU on ix86/linux.
            * config/i386/linux/md.c
            (init_md): Setup the FP unit to use double precision and not extended
            precision.
    
            * configure.ac: Check for fpu_control.h
    
            * configure, config/config.h.in: Regenerated.
    
            Fixes bug reported by Daniel Bonniot <Daniel.Bonniot@inria.fr>

--- config/i386/linux/md.c
+++ config/i386/linux/md.c
@@ -20,11 +20,31 @@
 #include <features.h>
 #endif
 
+#if defined(HAVE_FPU_CONTROL_H)
+#include <fpu_control.h>
+#endif
+
 void
 init_md(void)
 {
 #if defined(M_MMAP_MAX) && defined(HAVE_MALLOPT)
 	mallopt(M_MMAP_MAX, 0);
+#endif
+
+#if defined(HAVE_FPU_CONTROL_H)
+	/* See http://www.srware.com/linux_numerics.txt */
+        /*
+	  This puts the X86 FPU in 64-bit precision mode.  The default
+	  under Linux is to use 80-bit mode, which produces subtle
+	  differences from FreeBSD and other systems, eg,
+	  (int)(1000*atof("0.3")) is 300 in 64-bit mode, 299 in 80-bit
+	  mode.
+        */
+        fpu_control_t cw;
+        _FPU_GETCW(cw);
+        cw &= ~_FPU_EXTENDED;
+        cw |= _FPU_DOUBLE;
+        _FPU_SETCW(cw);
 #endif
 }
 
