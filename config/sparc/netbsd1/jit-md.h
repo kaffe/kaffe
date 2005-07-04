516c13feee9d2a824a892dd24ec4559033680b79
robilad <robilad>
2005-07-04 00:41:41 +0000

    Fixes for accessor macros on sparc-netbsd2
    2005-07-04  Riccardo Mottola  <rmottola@users.sf.net>
    
            * config/sparc/netbsd1/jit-md.h,
            config/sparc/netbsd1/md.h:
            Changed macros to use ucontext_t instead of sigcontext.
            Also unified them to use macros provided by netbsd that
            are portable among different platforms to get SP and PC.

--- config/sparc/netbsd1/jit-md.h
+++ config/sparc/netbsd1/jit-md.h
@@ -5,12 +5,18 @@
  * Copyright (c) 1996, 1997
  *	Transvirtual Technologies, Inc.  All rights reserved.
  *
+ * Copyright (c) 2005
+ *      The kaffe.org's developers. See ChangeLog for details.
+ *
  * See the file "license.terms" for information on usage and redistribution 
  * of this file. 
  */
 
 #ifndef __sparc_netbsd_jit_md_h
 #define __sparc_netbsd_jit_md_h
+
+/* mcontext.h gives us macros to access ucontext/mcontext */
+#include <sys/ucontext.h>
 
 /**/
 /* Include common information. */
@@ -23,11 +29,13 @@
 
 /* Function prototype for signal handlers */
 #define EXCEPTIONPROTO                                                  \
-        int sig, int code, struct sigcontext* ctx
+        int sig, int code, ucontext_t* ctx
 
 /* Get the first exception frame from a signal handler */
 #define EXCEPTIONFRAME(f, c)                                            \
-        (f).retbp = (c)->sc_sp;                                         \
-        (f).retpc = (c)->sc_pc
+        (f).retbp = _UC_MACHINE_SP(((ucontext_t *)(c)));                \
+        (f).retpc = _UC_MACHINE_PC(((ucontext_t *)(c)))
 
-#endif
+
+
+#endif /* include guard */
--- config/sparc/netbsd1/md.h
+++ config/sparc/netbsd1/md.h
@@ -12,15 +12,29 @@
 #ifndef __sparc_netbsd1_md_h
 #define __sparc_netbsd1_md_h
 
+#include <signal.h>
+
 #include "sparc/common.h"
 #include "sparc/threads.h"
 
-#undef SA_SIGINFO
-#define SIGNAL_ARGS(sig, sc) int sig, int code, struct sigcontext* sc
-#define SIGNAL_CONTEXT_POINTER(scp) struct sigcontext* scp
-#define GET_SIGNAL_CONTEXT_POINTER(scp) (scp)
-#define SIGNAL_PC(scp) ((scp)->sc_pc)
-#define STACK_POINTER(scp) ((scp)->sc_sp)
+#include <sys/ucontext.h>
+
+/* older netbsd's could have this macro missing, so we provide it */
+#ifdef _UC_MACHINE_SP
+/* this is for 32bit it doesn't take in account 64bit */
+#define _UC_MACHINE_SP(uc)      ((uc)->uc_mcontext.__gregs[_REG_O6])
+#endif
+#ifdef _UC_MACHINE_PC
+#define _UC_MACHINE_PC(uc)      ((uc)->uc_mcontext.__gregs[_REG_PC])
+#endif
+
+#define SIGCONTEXT ucontext_t
+
+#define SIGNAL_ARGS(sig, sc) int sig, siginfo_t *__si, void *sc
+#define SIGNAL_CONTEXT_POINTER(scp) SIGCONTEXT *scp
+#define GET_SIGNAL_CONTEXT_POINTER(sc) (sc)
+#define SIGNAL_PC(scp) _UC_MACHINE_PC(((SIGCONTEXT *)(scp)))
+#define STACK_POINTER(scp) _UC_MACHINE_SP(((SIGCONTEXT *)(scp)))
 
 #if defined(TRANSLATOR)
 #include "jit-md.h"
