abea043c6557332c4cd6d7cb48e8bcc771152672
robilad <robilad>
2005-07-10 12:01:51 +0000

    Resyced with gnu classpath: cleanup

--- libraries/javalib/gnu/java/security/provider/SHA1PRNG.java
+++ libraries/javalib/gnu/java/security/provider/SHA1PRNG.java
@@ -1,5 +1,5 @@
 /* SHA1PRNG.java --- Secure Random SPI SHA1PRNG
-   Copyright (C) 1999, 2001, 2003 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2001, 2003, 2005 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
