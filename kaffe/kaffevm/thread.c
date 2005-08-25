f7c4a38c75ca29610716c574acf1f16b5e5f96b0
riccardo <riccardo>
2005-08-25 12:51:09 +0000

    added system includes for darwin

--- kaffe/kaffevm/thread.c
+++ kaffe/kaffevm/thread.c
@@ -14,6 +14,14 @@
 
 #include "config.h"
 
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h>
+#endif
+
 #if defined(HAVE_SYS_RESOURCE_H)
 #include <sys/resource.h>
 #endif
