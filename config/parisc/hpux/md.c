56b8e28629c1fc3b5968a6d9b72446bc7508a163
robilad <robilad>
2007-10-08 12:40:17 +0000

    2007-10-08  Dalibor Topic  <robilad@kaffe.org>
            * config/hpux/linux/md.c: Added empty file to fix build on
            hppa-hpux.
    
            * config/Makefile.am (EXTRA_DIST): Added parisc/hpux/md.c.

---
+++ config/parisc/hpux/md.c
@@ -0,0 +1,10 @@
+/*
+ * parisc/hpux/md.c
+ * HP-UX PA-RISC specific functions.
+ *
+ * Copyright (c) 2007
+ *      Kaffe.org contributors, see ChangeLogs for details.
+ *
+ * See the file "license.terms" for information on usage and redistribution
+ * of this file.
+ */     
