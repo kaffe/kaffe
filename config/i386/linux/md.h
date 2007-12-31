066df3d3cc7d802495aeeba966ced7bd3c8ac539
robilad <robilad>
2007-12-31 01:50:43 +0000

    small fix for jthreads on i386-linux
    2007-12-30  Dalibor Topic  <robilad@kaffe.org>
    
            * config/i386/linux/md.h (SP_OFFSET): Set to 3 to fix crashes with jthreads
            caused by bad SP_OFFSET. Still crashes for me later, though.

--- config/i386/linux/md.h
+++ config/i386/linux/md.h
@@ -18,6 +18,12 @@
 #include "i386/common.h"
 #include "i386/threads.h"
 
+/*
+ * Redefine stack pointer offset.
+ */
+#undef SP_OFFSET
+#define SP_OFFSET 3
+
 /**/
 /* Extra exception handling information. */
 /**/
